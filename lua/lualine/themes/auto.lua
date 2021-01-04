c101e9f27522f4e6d1ab946fe35d4ad9f6021c46
KXMN Developer <53506163+kxmndev@users.noreply.github.com>
2021-01-04 09:46:49 -0300

    Generates colorscheme automatically
    Generates theme automatically using syntax highlight of current colorscheme.
    The colors became perfect 95% of time... as it's only code doing the work, is good while have no official port or need to be fast, and being someone who uses too much colorschemes ;)

---
+++ lua/lualine/themes/auto.lua
@@ -0,0 +1,58 @@
+function _getHi( scope, syntaxlist )
+	local color
+	for i , syn in ipairs( syntaxlist  ) do
+		color = vim.api.nvim_command_output(
+			[[ execute 'hi ]] .. syn .. [[']]
+		)
+		if color:match( [[gui=reverse]] ) then
+			if scope == 'guifg' then scope = 'guibg' else scope = 'guifg' end
+		end
+		color = color:match( scope .. [[=(#?%w+)]]) or nil
+		if color then break end
+	end
+	return color or '#000000'
+end
+
+local _ = {
+	normal  = _getHi( 'guibg', {'PmenuSel', 'PmenuThumb', 'TabLineSel' } ),
+	insert  = _getHi( 'guifg', {'String', 'MoreMsg' } ),
+	replace = _getHi( 'guifg', {'Number', 'Type' } ),
+	visual  = _getHi( 'guifg', {'Special', 'Boolean', 'Constant' } ),
+	command = _getHi( 'guifg', {'Identifier' } ),
+	back1   = _getHi( 'guibg', {'Normal', 'StatusLineNC' } ),
+	fore    = _getHi( 'guifg', {'Normal', 'StatusLine' } ),
+	back2   = _getHi( 'guibg', {'StatusLine' } ),
+}
+
+local M = {
+	normal = {
+		a = { bg = _.normal,  fg = _.back1  },
+		b = { bg = _.back2,   fg = _.fore   },
+		c = { bg = _.back1,   fg = _.normal },
+	},
+	insert = {
+		a = { bg = _.insert,  fg = _.back1  },
+		b = { bg = _.back2,   fg = _.fore   },
+		c = { bg = _.back1,   fg = _.insert },
+	},
+	replace = {
+		a = { bg = _.replace, fg= _.back1   },
+		b = { bg = _.back2,   fg= _.fore    },
+		c = { bg = _.back1,   fg= _.replace },
+	},
+	visual = {
+		a = { bg = _.visual,  fg= _.back1   },
+		b = { bg = _.back2,   fg= _.fore    },
+		c = { bg = _.back1,   fg= _.visual  },
+	},
+	command = {
+		a = { bg = _.command, fg = _.back1  },
+		b = { bg = _.back2,   fg = _.fore   },
+		c = { bg = _.back1,   fg = _.command},
+	},
+}
+
+M.terminal = M.command
+M.inactive = M.normal
+
+return M
