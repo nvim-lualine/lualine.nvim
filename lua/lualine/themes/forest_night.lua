24db1a62e8121ae4bfaa72418f58f400a0816ef2
Brandon <brandonpadres31@gmail.com>
2021-01-04 15:15:03 -0800

    Added forest_night theme (#30)
    * Add forest_night theme
    
    Ported from forest-night's [lightline
    theme](https://github.com/sainnhe/forest-night/blob/master/autoload/lightline/colorscheme/forest_night.vim).
    
    * Add forest_night to THEMES.md

---
+++ lua/lualine/themes/forest_night.lua
@@ -0,0 +1,121 @@
+local forest_night = {}
+
+local colors = {
+  bg0 = "#323d43",
+  bg1 = "#3c474d",
+  bg3 = "#505a60",
+  fg = "#d8caac",
+  aqua = "#87c095",
+  green = "#a7c080",
+  orange = "#e39b7b",
+  purple = "#d39bb6",
+  red = "#e68183",
+  grey1 = "#868d80",
+}
+
+forest_night.normal = {
+  a = {
+    bg = colors.green,
+    fg = colors.bg0,
+  },
+  b = {
+    bg = colors.bg3,
+    fg = colors.fg,
+  },
+  c = {
+    bg = colors.bg1,
+    fg = colors.fg,
+  },
+}
+
+forest_night.insert = {
+  a = {
+    bg = colors.fg,
+    fg = colors.bg0,
+  },
+  b = {
+    bg = colors.bg3,
+    fg = colors.fg,
+  },
+  c = {
+    bg = colors.bg1,
+    fg = colors.fg,
+  },
+}
+
+forest_night.visual = {
+  a = {
+    bg = colors.red,
+    fg = colors.bg0,
+  },
+  b = {
+    bg = colors.bg3,
+    fg = colors.fg,
+  },
+  c = {
+    bg = colors.bg1,
+    fg = colors.fg,
+  },
+}
+
+forest_night.replace = {
+  a = {
+    bg = colors.orange,
+    fg = colors.bg0,
+  },
+  b = {
+    bg = colors.bg3,
+    fg = colors.fg,
+  },
+  c = {
+    bg = colors.bg1,
+    fg = colors.fg,
+  },
+}
+
+forest_night.command = {
+  a = {
+    bg = colors.aqua,
+    fg = colors.bg0,
+  },
+  b = {
+    bg = colors.bg3,
+    fg = colors.fg,
+  },
+  c = {
+    bg = colors.bg1,
+    fg = colors.fg,
+  },
+}
+
+forest_night.terminal = {
+  a = {
+    bg = colors.purple,
+    fg = colors.bg0,
+  },
+  b = {
+    bg = colors.bg3,
+    fg = colors.fg,
+  },
+  c = {
+    bg = colors.bg1,
+    fg = colors.fg,
+  },
+}
+
+forest_night.inactive = {
+  a = {
+    bg = colors.bg1,
+    fg = colors.grey1,
+  },
+  b = {
+    bg = colors.bg1,
+    fg = colors.grey1,
+  },
+  c = {
+    bg = colors.bg1,
+    fg = colors.grey1,
+  },
+}
+
+return forest_night
