0afda1974caf1121eed53a9315345a62df9ae1ce
shadmansaleh <shadmansaleh3@gmail.com>
2021-01-08 17:25:18 +0600

    Added icons for fileformat

--- lua/lualine/components/fileformat.lua
+++ lua/lualine/components/fileformat.lua
@@ -1,6 +1,12 @@
 local function fileformat()
-  local data = [[%{&ff}]]
-  return data
+  local ok, _ = pcall(require,'nvim-web-devicons')
+  if ok then   
+    local format = vim.bo.fileformat
+    if format == 'unix' then return ""
+    elseif format == 'dos' then return ""
+    elseif format == 'mac' then return "" end
+  end
+  return [[%{&ff}]]
 end
 
 return fileformat
