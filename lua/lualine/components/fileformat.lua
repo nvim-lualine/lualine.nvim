f8b2c0d1dfa8878e96491bb2667e3c7420e481df
shadmansaleh <shadmansaleh3@gmail.com>
2021-01-09 23:57:41 +0600

    Remove trailing whitspaces

--- lua/lualine/components/fileformat.lua
+++ lua/lualine/components/fileformat.lua
@@ -1,6 +1,6 @@
 local function fileformat()
   local ok, _ = pcall(require,'nvim-web-devicons')
-  if ok then   
+  if ok then
     local format = vim.bo.fileformat
     if format == 'unix' then return ""
     elseif format == 'dos' then return ""
