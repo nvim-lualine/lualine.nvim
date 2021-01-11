8ff22c39355c93bab3ce36411017b82471ac88a7
Shadman <shadmansaleh3@gmail.com>
2021-01-11 08:44:53 +0600

    Fixed signify component apearing on non repo buffer (#58)
    * Fixed signify component apearing on non repo buffer

--- lua/lualine/components/signify.lua
+++ lua/lualine/components/signify.lua
@@ -1,13 +1,11 @@
 local function signify()
    if vim.fn.exists('*sy#repo#get_stats') == 0 then return '' end
    local added, modified, removed = unpack(vim.fn['sy#repo#get_stats']())
-   if added == -1 then added = 0 end
-   if modified == -1 then modified = 0 end
-   if removed == -1 then removed = 0 end
+   if added == -1 then return '' end
    local data = {
-    '+', added,
-    '~', modified,
-    '-', removed
+    '+'..added,
+    '-'..removed,
+    '~'..modified,
    }
   return table.concat(data, ' ')
 end
