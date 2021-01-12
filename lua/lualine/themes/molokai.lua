7ca7d3ec844879832ae9b1f76170363cb7d057db
shadmansaleh <shadmansaleh3@gmail.com>
2021-01-12 17:25:29 +0600

    Added molokai theme
    ported from lightline

---
+++ lua/lualine/themes/molokai.lua
@@ -0,0 +1,42 @@
+local gruvbox = {}
+
+local colors = {
+  black  = { '#232526', 233 },
+  gray   = { '#808080', 244 },
+  white  = { '#f8f8f2', 234 },
+  cyan   = { '#66d9ef', 81  },
+  green  = { '#a6e22e', 118 },
+  orange = { '#ef5939', 166 },
+  pink   = { '#f92672', 161 },
+  red    = { '#ff0000', 160 },
+  yellow = { '#e6db74', 229 },
+
+
+}
+
+gruvbox.normal = {
+  a = { fg = colors.black, bg = colors.cyan , gui = 'bold', },
+  b = { fg = colors.black, bg = colors.pink , },
+  c = { fg = colors.orange, bg = colors.black , }
+}
+
+gruvbox.insert = {
+  a = { fg = colors.black, bg = colors.green , gui = 'bold', },
+}
+
+
+gruvbox.visual = {
+  a = { fg = colors.black, bg = colors.yellow , gui = 'bold', },
+}
+
+gruvbox.replace = {
+  a = { fg = colors.black, bg = colors.red , gui = 'bold', },
+}
+
+gruvbox.inactive = {
+  a = { fg = colors.pink, bg = colors.black , gui = 'bold', },
+  b = { fg = colors.white, bg = colors.pink , },
+  c = { fg = colors.gray, bg = colors.black , },
+}
+
+return gruvbox
