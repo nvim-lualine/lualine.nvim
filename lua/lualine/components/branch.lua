a4dea6c2bedb0e35a91fc75619f18daea39710bf
shadmansaleh <shadmansaleh3@gmail.com>
2021-01-13 11:55:38 +0600

    Insted of calling git branch is read from HEAD

--- lua/lualine/components/branch.lua
+++ lua/lualine/components/branch.lua
@@ -1,33 +1,65 @@
-local async = require('lualine.async')
-
 local git_branch
 
-local get_git_branch = async:new({
-  cmd = 'git branch --show-current',
-  on_stdout = function(_, data)
-    if data then
-      git_branch = data:gsub('\n', '')
-    end
-  end,
-  on_stderr = function (_, data)
-    if data then
-      if data:find("fatal: not a git repository") then
-        git_branch = ''
+local known_git_dirs = {}
+local sep = package.config:sub(1,1)
+
+local function exists(path)
+  local f = io.open(path)
+  if not f then return false, false end
+  local _, _, code = f:read()
+  return true, code == 21
+end
+
+local function find_git_dir()
+  local file_path = vim.fn.expand('%:p:h')
+  -- See if we already know where it is
+  if known_git_dirs[file_path] then
+    return known_git_dirs[file_path]
+  end
+
+  -- path seperator is not in the end add it
+  local dir = file_path..sep
+  -- do we need to iterate more than that?
+  local n = 30
+  while dir and n > 0 do
+    n = n - 1
+    local git_dir = dir..'.git'
+    local is_available, is_directory = exists(git_dir)
+    -- if exists(git_dir) then
+    if is_available then
+      if not is_directory then
+        -- separate git-dir is used
+        local git_file = io.open(git_dir)
+        git_dir = git_file:read()
+        git_dir = git_dir:match("gitdir: (.+)")
       end
+      -- store for reuse
+      known_git_dirs[file_path] = git_dir
+      return git_dir
     end
-  end,
-})
+    dir = dir:match("(.*"..sep..").+$") -- "(.*/).+$"
+  end
+end
+
+local function get_git_head()
+  local git_dir = find_git_dir()
+  if git_dir then
+    local head_file = io.open(git_dir..sep..'HEAD')
+    if head_file then
+      local HEAD = head_file:read()
+      local branch = HEAD:match('ref: refs/heads/(.+)')
+      if branch then return branch end
+      return HEAD:sub(1,6)
+    end
+  end
+  return ''
+end
 
 local timer = vim.loop.new_timer()
 timer:start(0, 1000, vim.schedule_wrap(function()
-  local cur_dir = vim.fn.getcwd()
-  local buffer_working_directory = vim.fn.expand("%:p:h")
-  local status, _ = pcall(vim.api.nvim_set_current_dir, buffer_working_directory)
-  if status == true then
-    get_git_branch:start()
-    vim.api.nvim_set_current_dir(cur_dir)
-  end
+  git_branch = get_git_head()
 end))
+
 
 local function branch()
   if not git_branch or #git_branch == 0 then return '' end
