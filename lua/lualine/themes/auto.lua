8f778e775293cb0af5ff6fe5c357bc5c2d25bd03
shadmansaleh <shadmansaleh3@gmail.com>
2021-02-23 11:43:29 +0600

    retab

--- lua/lualine/themes/auto.lua
+++ lua/lualine/themes/auto.lua
@@ -11,40 +11,40 @@ local brightness_modifier_parameter = 10
 -- retrives color value from highlight group name in syntax_list
 -- first present highlight is returned
 local function getHi( scope, syntaxlist )
-	for _ , highlight_name in pairs( syntaxlist  ) do
-		if vim.fn.hlexists(highlight_name) ~= 0 then
-			local color = utils.extract_highlight_colors(highlight_name)
+  for _ , highlight_name in pairs( syntaxlist  ) do
+    if vim.fn.hlexists(highlight_name) ~= 0 then
+      local color = utils.extract_highlight_colors(highlight_name)
       if color.reverse then
         if scope == 'guibg' then scope = 'guifg' else scope = 'guibg' end
       end
       if color[scope] then return color[scope] end
-		end
-	end
-	return '#000000'
+    end
+  end
+  return '#000000'
 end
 
 -- truns #rrggbb -> { red, green, blue }
 local function rgb_str2num(rgb_color_str)
-	if rgb_color_str:find('#') == 1 then rgb_color_str = rgb_color_str:sub(2, #rgb_color_str) end
-	local red = tonumber(rgb_color_str:sub(1,2), 16)
-	local green = tonumber(rgb_color_str:sub(3,4), 16)
-	local blue = tonumber(rgb_color_str:sub(5,6), 16)
-	return { red = red, green = green, blue = blue, }
+  if rgb_color_str:find('#') == 1 then rgb_color_str = rgb_color_str:sub(2, #rgb_color_str) end
+  local red = tonumber(rgb_color_str:sub(1,2), 16)
+  local green = tonumber(rgb_color_str:sub(3,4), 16)
+  local blue = tonumber(rgb_color_str:sub(5,6), 16)
+  return { red = red, green = green, blue = blue, }
 end
 
 -- turns { red, green, blue } -> #rrggbb
 local function rgb_num2str(rgb_color_num)
-	local rgb_color_str = string.format('#%02x%02x%02x', rgb_color_num.red,
-		rgb_color_num.green, rgb_color_num.blue)
-	return rgb_color_str
+  local rgb_color_str = string.format('#%02x%02x%02x', rgb_color_num.red,
+    rgb_color_num.green, rgb_color_num.blue)
+  return rgb_color_str
 end
 
 -- returns brightness lavel of color in range 0 to 1
 -- arbitary value it's basicaly an weighted average
 local function get_color_brightness(rgb_color)
-	local color = rgb_str2num(rgb_color)
-	local brightness = (color.red * 2 + color.green * 3 + color.blue) / 6
-	return brightness / 256
+  local color = rgb_str2num(rgb_color)
+  local brightness = (color.red * 2 + color.green * 3 + color.blue) / 6
+  return brightness / 256
 end
 
 -- returns average of colors in range 0 to 1
@@ -63,20 +63,20 @@ end
 
 -- changes braghtness of rgb_color by percentage
 local function brightness_modifier(rgb_color, parcentage)
-	local color = rgb_str2num(rgb_color)
-	color.red = clamp(color.red + (color.red * parcentage / 100), 0, 255)
-	color.green = clamp(color.green + (color.green * parcentage / 100), 0, 255)
-	color.blue = clamp(color.blue + (color.blue * parcentage / 100), 0, 255)
-	return rgb_num2str(color)
+  local color = rgb_str2num(rgb_color)
+  color.red = clamp(color.red + (color.red * parcentage / 100), 0, 255)
+  color.green = clamp(color.green + (color.green * parcentage / 100), 0, 255)
+  color.blue = clamp(color.blue + (color.blue * parcentage / 100), 0, 255)
+  return rgb_num2str(color)
 end
 
 -- changes contrast of rgb_color by amount
 local function contrast_modifier(rgb_color, amount)
-	local color = rgb_str2num(rgb_color)
-	color.red = clamp(color.red + amount, 0, 255)
-	color.green = clamp(color.green + amount, 0, 255)
-	color.blue = clamp(color.blue + amount, 0, 255)
-	return rgb_num2str(color)
+  local color = rgb_str2num(rgb_color)
+  color.red = clamp(color.red + amount, 0, 255)
+  color.green = clamp(color.green + amount, 0, 255)
+  color.blue = clamp(color.blue + amount, 0, 255)
+  return rgb_num2str(color)
 end
 
 -- Changes brightness of foreground color to achive contrast
@@ -99,14 +99,14 @@ end
 
 -- Get the colors to create theme
 local colors = {
-	normal  = getHi( 'guibg', {'PmenuSel', 'PmenuThumb', 'TabLineSel' } ),
-	insert  = getHi( 'guifg', {'String', 'MoreMsg' } ),
-	replace = getHi( 'guifg', {'Number', 'Type' } ),
-	visual  = getHi( 'guifg', {'Special', 'Boolean', 'Constant' } ),
-	command = getHi( 'guifg', {'Identifier' } ),
-	back1   = getHi( 'guibg', {'Normal', 'StatusLineNC' } ),
-	fore    = getHi( 'guifg', {'Normal', 'StatusLine' } ),
-	back2   = getHi( 'guibg', {'StatusLine' } ),
+  normal  = getHi( 'guibg', {'PmenuSel', 'PmenuThumb', 'TabLineSel' } ),
+  insert  = getHi( 'guifg', {'String', 'MoreMsg' } ),
+  replace = getHi( 'guifg', {'Number', 'Type' } ),
+  visual  = getHi( 'guifg', {'Special', 'Boolean', 'Constant' } ),
+  command = getHi( 'guifg', {'Identifier' } ),
+  back1   = getHi( 'guibg', {'Normal', 'StatusLineNC' } ),
+  fore    = getHi( 'guifg', {'Normal', 'StatusLine' } ),
+  back2   = getHi( 'guibg', {'StatusLine' } ),
 }
 
 
@@ -118,36 +118,36 @@ if get_color_brightness(utils.extract_highlight_colors('Normal', 'guibg')) > 0.5
 end
 
 for name, color in pairs(colors) do
-	colors[name] = brightness_modifier(color, brightness_modifier_parameter)
+  colors[name] = brightness_modifier(color, brightness_modifier_parameter)
 end
 
 -- basic theme defination
 local M = {
-	normal = {
-		a = { bg = colors.normal,  fg = colors.back1, gui='bold' },
-		b = { bg = colors.back1,   fg = colors.normal            },
-		c = { bg = colors.back2,   fg = colors.fore              },
-	},
-	insert = {
-		a = { bg = colors.insert,  fg = colors.back1, gui='bold' },
-		b = { bg = colors.back1,   fg = colors.insert            },
-		c = { bg = colors.back2,   fg = colors.fore              },
-	},
-	replace = {
-		a = { bg = colors.replace, fg= colors.back1, gui='bold' },
-		b = { bg = colors.back1,   fg= colors.replace           },
-		c = { bg = colors.back2,   fg= colors.fore              },
-	},
-	visual = {
-		a = { bg = colors.visual,  fg= colors.back1, gui='bold' },
-		b = { bg = colors.back1,   fg= colors.visual            },
-		c = { bg = colors.back2,   fg= colors.fore              },
-	},
-	command = {
-		a = { bg = colors.command, fg = colors.back1, gui='bold' },
-		b = { bg = colors.back1,   fg = colors.command           },
-		c = { bg = colors.back2,   fg = colors.fore              },
-	},
+  normal = {
+    a = { bg = colors.normal,  fg = colors.back1, gui='bold' },
+    b = { bg = colors.back1,   fg = colors.normal            },
+    c = { bg = colors.back2,   fg = colors.fore              },
+  },
+  insert = {
+    a = { bg = colors.insert,  fg = colors.back1, gui='bold' },
+    b = { bg = colors.back1,   fg = colors.insert            },
+    c = { bg = colors.back2,   fg = colors.fore              },
+  },
+  replace = {
+    a = { bg = colors.replace, fg= colors.back1, gui='bold' },
+    b = { bg = colors.back1,   fg= colors.replace           },
+    c = { bg = colors.back2,   fg= colors.fore              },
+  },
+  visual = {
+    a = { bg = colors.visual,  fg= colors.back1, gui='bold' },
+    b = { bg = colors.back1,   fg= colors.visual            },
+    c = { bg = colors.back2,   fg= colors.fore              },
+  },
+  command = {
+    a = { bg = colors.command, fg = colors.back1, gui='bold' },
+    b = { bg = colors.back1,   fg = colors.command           },
+    c = { bg = colors.back2,   fg = colors.fore              },
+  },
 }
 
 M.terminal = M.command
