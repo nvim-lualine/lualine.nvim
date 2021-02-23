78ed0c2624db7f641641905c368aa2bb2d1b16c5
shadmansaleh <shadmansaleh3@gmail.com>
2021-02-23 12:16:34 +0600

    Improve signify stub : Now meesage is shown after 3 seconds

--- lua/lualine/components/signify.lua
+++ lua/lualine/components/signify.lua
@@ -1,2 +1,8 @@
-print('lualine.nvim: Signify component has been renamed to diff please change it in your configuration.')
+vim.fn.timer_start(3000, function()
+if vim.api.nvim_echo then
+  vim.api.nvim_echo({{'lualine.nvim: Signify component has been renamed to diff please change it in your configuration.', 'WarningMsg'}}, true, {})
+else
+  print('lualine.nvim: Signify component has been renamed to diff please change it in your configuration.', 'ErrorMsg')
+end
+end)
 return require'lualine.components.diff'
