64a378d670bf571f46922617a4d330308ca40f3e
hoob3rt <pelczarskihubert@gmail.com>
2021-03-11 00:14:37 +0100

    feat: added fugitive extension

---
+++ lua/lualine/extensions/fugitive.lua
@@ -0,0 +1,16 @@
+-- Copyright (c) 2020-2021 hoob3rt
+-- MIT license, see LICENSE for more details.
+
+local M = {}
+
+M.sections = {
+  lualine_a = { 'FugitiveHead' },
+}
+
+M.inactive_sections = {
+  lualine_a = { 'FugitiveHead' },
+}
+
+M.filetypes = { 'fugitive' }
+
+return M
