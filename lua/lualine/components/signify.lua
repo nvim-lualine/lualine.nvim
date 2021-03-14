a0346824aa2abdc65a13c943ccfd950d4aa5babc
hoob3rt <pelczarskihubert@gmail.com>
2021-03-14 14:49:04 +0100

    refactor: removed all signify file

--- lua/lualine/components/signify.lua
+++
@@ -1,8 +0,0 @@
-vim.fn.timer_start(3000, function()
-if vim.api.nvim_echo then
-  vim.api.nvim_echo({{'lualine.nvim: Signify component has been renamed to diff please change it in your configuration.', 'WarningMsg'}}, true, {})
-else
-  print('lualine.nvim: Signify component has been renamed to diff please change it in your configuration.', 'ErrorMsg')
-end
-end)
-return require'lualine.components.diff'
