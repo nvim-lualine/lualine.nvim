7c8ddf307b28530107ab604f7c212ec1176561b9
Shadman <shadmansaleh3@gmail.com>
2021-03-22 20:29:37 +0600

    refactor: highlight improvements
    * Refactor highlight restoring after ColorScheme
    
    Previously custom highlights/ component highlights had to call
    expand_set_theme to make them restored . That's unintuative . Now any
    highlight created by lualine.highlight.highlight() will be restored :)
    
    * Renamed component -> var_name | Proper BugFix

--- lua/lualine/init.lua
+++ lua/lualine/init.lua
@@ -1,6 +1,5 @@
 -- Copyright (c) 2020-2021 hoob3rt
 -- MIT license, see LICENSE for more details.
-local utils = require('lualine.utils.utils')
 local utils_section = require('lualine.utils.section')
 local highlight = require('lualine.highlight')
 local config = require('lualine.defaults')
@@ -69,11 +68,9 @@ local function load_special_components(component)
       -- filters g portion from g:var
       local scope = component:match('[gvtwb]?o?')
       -- filters var portion from g:var
-      -- For some reason overwriting component var from outer scope causes the
-      -- component not to work . So creating a new local name component to use:/
-      local component = component:sub(#scope + 2, #component)
+      local var_name = component:sub(#scope + 2, #component)
       -- Displays nothing when veriable aren't present
-      local return_val = vim[scope][component]
+      local return_val = vim[scope][var_name]
       if return_val == nil then return '' end
       local ok
       ok, return_val = pcall(tostring, return_val)
@@ -122,13 +119,9 @@ local function component_loader(component)
     end
     -- set custom highlights
     if component.color then
-      local function update_color()
-        component.color_highlight = highlight.create_component_highlight_group(
-                                        component.color,
-                                        component.component_name, component)
-      end
-      update_color()
-      utils.expand_set_theme(update_color)
+      component.color_highlight = highlight.create_component_highlight_group(
+                                      component.color,
+                                      component.component_name, component)
     end
   end
 end
@@ -160,27 +153,6 @@ local function load_extensions()
     load_sections(local_extension.inactive_sections)
     config.extensions[index] = local_extension
   end
-end
-
-local function lualine_set_theme()
-  if type(config.options.theme) == 'string' then
-    config.options.theme = require('lualine.themes.' .. config.options.theme)
-    -- change the theme table in component so their custom
-    -- highlights can reflect theme change
-    local function reset_component_theme(sections)
-      for _, section in pairs(sections) do
-        for _, component in pairs(section) do
-          if type(component) == 'table' then
-            component.theme = config.options.theme
-          end
-        end
-      end
-    end
-    reset_component_theme(config.sections)
-    reset_component_theme(config.inactive_sections)
-  end
-  utils.clear_highlights()
-  highlight.create_highlight_groups(config.options.theme)
 end
 
 local function statusline(sections, is_focused)
@@ -296,12 +268,14 @@ end
 local function tabline() return statusline(config.tabline, true) end
 
 local function setup_theme()
-  lualine_set_theme()
-  _G.lualine_set_theme = lualine_set_theme
+  if type(config.options.theme) == 'string' then
+    config.options.theme = require('lualine.themes.' .. config.options.theme)
+  end
+  highlight.create_highlight_groups(config.options.theme)
   vim.api.nvim_exec([[
   augroup lualine
   autocmd!
-  autocmd ColorScheme * call v:lua.lualine_set_theme()
+  autocmd ColorScheme * lua require'lualine.utils.utils'.reload_highlights()
   augroup END
   ]], false)
 end
