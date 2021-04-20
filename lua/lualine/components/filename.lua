09914256319301da108ff81231ddd9c1b9945214
Tim Bedard <tpbedard@gmail.com>
2021-04-19 22:19:39 -0500

    single quotes -> double quotes

--- lua/lualine/components/filename.lua
+++ lua/lualine/components/filename.lua
@@ -1,12 +1,12 @@
 -- Copyright (c) 2020-2021 shadmansaleh
 -- MIT license, see LICENSE for more details.
-local FileName = require("lualine.component"):new()
+local FileName = require('lualine.component'):new()
 
 FileName.new = function(self, options, child)
   local new_instance = self._parent:new(options, child or FileName)
-  local default_symbols = {modified = "[+]", readonly = "[-]"}
+  local default_symbols = {modified = '[+]', readonly = '[-]'}
   new_instance.options.symbols =
-    vim.tbl_extend("force", default_symbols, new_instance.options.symbols or {})
+    vim.tbl_extend('force', default_symbols, new_instance.options.symbols or {})
 
   -- setting defaults
   if new_instance.options.file_status == nil then
@@ -23,15 +23,15 @@ FileName.new = function(self, options, child)
 end
 
 FileName.update_status = function(self)
-  local data = vim.fn.expand("%:p")
+  local data = vim.fn.expand('%:p')
   if not self.options.full_path then
-    data = vim.fn.expand("%:t")
+    data = vim.fn.expand('%:t')
   elseif self.options.shorten then
-    data = vim.fn.expand("%:~:.")
+    data = vim.fn.expand('%:~:.')
   end
 
-  if data == "" then
-    data = "[No Name]"
+  if data == '' then
+    data = '[No Name]'
   elseif vim.fn.winwidth(0) <= 84 or #data > 40 then
     data = vim.fn.pathshorten(data)
   end
