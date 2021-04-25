6ba2b80b594c3ead11ab9bd1dbc94c0b4ea46c33
agarof <18119032+agarof@users.noreply.github.com>
2021-04-26 00:24:29 +0200

    Fix themes incorrectly setting the bold attribute (#194)

--- lua/lualine/themes/ayu_mirage.lua
+++ lua/lualine/themes/ayu_mirage.lua
@@ -17,25 +17,25 @@ local colors = {
 
 local ayu_mirage = {
   visual = {
-    a = {fg = colors.color2, bg = colors.color3, 'bold'},
+    a = {fg = colors.color2, bg = colors.color3, gui = 'bold'},
     b = {fg = colors.color4, bg = colors.color5}
   },
   replace = {
-    a = {fg = colors.color2, bg = colors.color8, 'bold'},
+    a = {fg = colors.color2, bg = colors.color8, gui = 'bold'},
     b = {fg = colors.color4, bg = colors.color5}
   },
   inactive = {
     c = {fg = colors.color4, bg = colors.color2},
-    a = {fg = colors.color4, bg = colors.color5, 'bold'},
+    a = {fg = colors.color4, bg = colors.color5, gui = 'bold'},
     b = {fg = colors.color4, bg = colors.color5}
   },
   normal = {
     c = {fg = colors.color9, bg = colors.color2},
-    a = {fg = colors.color2, bg = colors.color10, 'bold'},
+    a = {fg = colors.color2, bg = colors.color10, gui = 'bold'},
     b = {fg = colors.color4, bg = colors.color5}
   },
   insert = {
-    a = {fg = colors.color2, bg = colors.color13, 'bold'},
+    a = {fg = colors.color2, bg = colors.color13, gui = 'bold'},
     b = {fg = colors.color4, bg = colors.color5}
   }
 }
