2e86c72d15de49fdd3c1adf10c748f85b17904b5
hubert <pelczarskihubert@gmail.com>
2021-05-11 00:22:48 +0200

    feat: mode component shortening

--- lua/lualine/components/mode.lua
+++ lua/lualine/components/mode.lua
@@ -3,6 +3,11 @@
 local Mode = require('lualine.component'):new()
 local get_mode = require('lualine.utils.mode').get_mode
 
-Mode.update_status = get_mode
+Mode.update_status = function()
+  local data = get_mode()
+  local windwidth = vim.fn.winwidth(0)
+  if windwidth <= 84 then return data:sub(1,1) end
+  return data
+end
 
 return Mode
