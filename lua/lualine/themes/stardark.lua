a834e49cc3d5b9f748704e80447a7fd1a66b86c9
ashincoder <ashinant15@gmail.com>
2021-07-16 11:36:00 +0530

    white fg

--- lua/lualine/themes/stardark.lua
+++ lua/lualine/themes/stardark.lua
@@ -2,16 +2,16 @@
 -- MIT license, see LICENSE for more details.
 -- LuaFormatter off
 local colors = {
-	black     = "#282828",
-	white     = "#ebdbb2",
-	red       = "#ff1a1a",
-	green     = "#669900",
-	blue      = "#5396c2",
-  dark      = "#11121D",
-	yellow    = "#e6e600",
-	gray      = "#4d1919",
-	pink      = "#ad3838",
-	darkgray  = "#281b10",
+	black = "#282828",
+	white = "#ebdbb2",
+	red = "#ff1a1a",
+	green = "#669900",
+	blue = "#5396c2",
+	dark = "#11121D",
+	yellow = "#e6e600",
+	gray = "#4d1919",
+	pink = "#ad3838",
+	darkgray = "#281b10",
 	lightgray = "#363636",
 	inactivegray = "#656777",
 }
@@ -23,22 +23,22 @@ return {
 		c = { bg = colors.darkgray, fg = colors.white },
 	},
 	insert = {
-		a = { bg = colors.blue, fg = colors.black, gui = "bold" },
+		a = { bg = colors.blue, fg = colors.white, gui = "bold" },
 		b = { bg = colors.gray, fg = colors.white },
 		c = { bg = colors.lightgray, fg = colors.white },
 	},
 	visual = {
-		a = { bg = colors.yellow, fg = colors.black, gui = "bold" },
+		a = { bg = colors.yellow, fg = colors.white, gui = "bold" },
 		b = { bg = colors.lightgray, fg = colors.white },
 		c = { bg = colors.gray, fg = colors.white },
 	},
 	replace = {
-		a = { bg = colors.red, fg = colors.black, gui = "bold" },
+		a = { bg = colors.red, fg = colors.white, gui = "bold" },
 		b = { bg = colors.lightgray, fg = colors.white },
 		c = { bg = colors.black, fg = colors.white },
 	},
 	command = {
-		a = { bg = colors.green, fg = colors.black, gui = "bold" },
+		a = { bg = colors.green, fg = colors.white, gui = "bold" },
 		b = { bg = colors.lightgray, fg = colors.white },
 		c = { bg = colors.lightgray, fg = colors.white },
 	},
