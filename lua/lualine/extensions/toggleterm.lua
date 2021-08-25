a690ee2ad9c0fcf9356173d4b94a1f2d587cf916
Daniel Enrique <dcordb97@gmail.com>
2021-08-25 07:35:20 -0400

    Add # symbol.

--- lua/lualine/extensions/toggleterm.lua
+++ lua/lualine/extensions/toggleterm.lua
@@ -1,7 +1,7 @@
 -- Copyright (c) 2020-2021 hoob3rt
 -- MIT license, see LICENSE for more details.
 
-local function toggleterm_statusline() return 'ToggleTerm ' .. vim.b.toggle_number end
+local function toggleterm_statusline() return 'ToggleTerm #' .. vim.b.toggle_number end
 
 local M = {}
 
