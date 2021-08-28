ba8fb842734abdfbd155bb48b3990f13ef990e8a
Ashish Panigrahi <ashish.panigrahi@protonmail.com>
2021-08-28 19:45:06 +0530

    added powerline-dark theme

---
+++ lua/lualine/themes/powerline_dark.lua
@@ -0,0 +1,54 @@
+-- Copyright (c) 2021 Ashish Panigrahi
+-- MIT license, see LICENSE for more details.
+-- LuaFormatter off
+local colors = {
+  black        = '#202020',
+  neon         = '#DFFF00',
+  white        = '#FFFFFF',
+  green        = '#00D700',
+  purple       = '#5F005F',
+  blue         = '#00DFFF',
+  darkblue     = '#00005F',
+  navyblue     = '#000080',
+  brightgreen  = '#9CFFD3',
+  gray         = '#444444',
+  darkgray     = '#3c3836',
+  lightgray    = '#504945',
+  inactivegray = '#7c6f64',
+  orange       = '#FFAF00',
+  red          = '#5F0000',
+  brightorange = '#C08A20',
+}
+-- LuaFormatter on
+return {
+  normal = {
+    a = {bg = colors.neon, fg = colors.black, gui = 'bold'},
+    b = {bg = colors.gray, fg = colors.white},
+    c = {bg = colors.black, fg = colors.brightgreen}
+  },
+  insert = {
+    a = {bg = colors.blue, fg = colors.darkblue, gui = 'bold'},
+    b = {bg = colors.navyblue, fg = colors.white},
+    c = {bg = colors.purple, fg = colors.white}
+  },
+  visual = {
+    a = {bg = colors.orange, fg = colors.black, gui = 'bold'},
+    b = {bg = colors.darkgray, fg = colors.white},
+    c = {bg = colors.red, fg = colors.white}
+  },
+  replace = {
+    a = {bg = colors.brightorange, fg = colors.black, gui = 'bold'},
+    b = {bg = colors.lightgray, fg = colors.white},
+    c = {bg = colors.black, fg = colors.white}
+  },
+  command = {
+    a = {bg = colors.green, fg = colors.black, gui = 'bold'},
+    b = {bg = colors.darkgray, fg = colors.white},
+    c = {bg = colors.black, fg = colors.brightgreen}
+  },
+  inactive = {
+    a = {bg = colors.darkgray, fg = colors.gray, gui = 'bold'},
+    b = {bg = colors.darkgray, fg = colors.gray},
+    c = {bg = colors.darkgray, fg = colors.gray}
+  }
+}
