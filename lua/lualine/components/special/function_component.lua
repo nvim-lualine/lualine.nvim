a643a24d9437e3499e3d813c1d63e5e6acff9e7d
shadmansaleh <13149513+shadmansaleh@users.noreply.github.com>
2021-09-03 18:19:00 +0600

    enhance: improve function component
    - provide self as 1st argument
    - handle error in function call
    - convert the result to string by default

--- lua/lualine/components/special/function_component.lua
+++ lua/lualine/components/special/function_component.lua
@@ -2,7 +2,14 @@ local FunctionComponent = require('lualine.component'):new()
 
 FunctionComponent.update_status = function(self, is_focused)
   -- 1st element in options table is the function provided by config
-  return self.options[1](is_focused)
+  local ok, retval
+  ok, retval = pcall(self.options[1], self, is_focused)
+  if not ok then return '' end
+  if type(retval) ~= 'string' then
+    ok, retval = pcall(tostring, retval)
+    if not ok then return '' end
+  end
+  return retval
 end
 
 return FunctionComponent
