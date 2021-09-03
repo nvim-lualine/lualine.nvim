8b4f70817c0a8d548adc1b6254627e91c1c27466
shadmansaleh <13149513+shadmansaleh@users.noreply.github.com>
2021-09-03 15:59:00 +0600

    Refactor: Rename lualine_conifg.lua -> lualine/config.lua
    This is loaded by lualine require so the peroformence will not be effected.

--- lua/tests/spec/config_spec.lua
+++ lua/tests/spec/config_spec.lua
@@ -1,7 +1,7 @@
 local eq = assert.are.same
 
 describe('config parsing', function()
-  local config_module = require 'lualine_config'
+  local config_module = require 'lualine.config'
 
   describe('options', function()
     describe('icons_enabled', function()
