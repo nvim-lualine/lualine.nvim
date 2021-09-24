2225727a3559b725ddaeddc3e855a51397f2a091
DanisDGK <danisdgk@gmail.com>
2021-09-24 19:50:15 +0300

    Add Sunbather theme

---
+++ lua/lualine/themes/sunbather.lua
@@ -0,0 +1,40 @@
+local colors = {
+  black           = "#000000",
+  medium_gray     = "#767676",
+  subtle_black    = "#121212",
+  light_black     = "#262626",
+  lightest_gray   = "#EEEEEE",
+  dark_pink       = "#ff5f87",
+}
+return {
+  normal = {
+    a = {bg = colors.dark_pink, fg = colors.black, gui = 'bold'},
+    b = {bg = colors.black, fg = colors.dark_pink},
+    c = {bg = colors.subtle_black, fg = colors.lightest_gray}
+  },
+  insert = {
+    a = {bg = colors.dark_pink, fg = colors.black, gui = 'bold'},
+    b = {bg = colors.black, fg = colors.dark_pink},
+    c = {bg = colors.subtle_black, fg = colors.lightest_gray}
+  },
+  visual = {
+    a = {bg = colors.medium_gray, fg = colors.black, gui = 'bold'},
+    b = {bg = colors.black, fg = colors.medium_gray},
+    c = {bg = colors.subtle_black, fg = colors.lightest_gray}
+  },
+  replace = {
+    a = {bg = colors.dark_pink, fg = colors.black, gui = 'bold'},
+    b = {bg = colors.black, fg = colors.dark_pink},
+    c = {bg = colors.subtle_black, fg = colors.lightest_gray}
+  },
+  command = {
+    a = {bg = colors.lightest_gray, fg = colors.black, gui = 'bold'},
+    b = {bg = colors.black, fg = colors.lightest_gray},
+    c = {bg = colors.subtle_black, fg = colors.lightest_gray}
+  },
+  inactive = {
+    a = {bg = colors.light_black, fg = colors.lightest_gray, gui = 'bold'},
+    b = {bg = colors.light_black, fg = colors.lightest_gray},
+    c = {bg = colors.light_black, fg = colors.lightest_gray}
+  }
+}
