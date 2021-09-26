a2184780436a8d1def42d4e6020b6ecaee53fb2a
allumik <alvinmeltsov@gmail.com>
2021-09-26 15:23:37 +0300

    Create modus_operandi.lua
    Init modus operandi theme, based on vivendi theme by @ronniedroid

---
+++ lua/lualine/themes/modus_operandi.lua
@@ -0,0 +1,54 @@
+-- Copyright (c) 2020-2021 allumik
+-- MIT license, see LICENSE for more details.
+-- LuaFormatter off
+local colors = {
+  black = '#000000',
+  white = '#ffffff',
+  red = '#7f1010',
+  green = '#104410',
+  blue = '#003497',
+  magenta = '#752f50',
+  cyan = '#005077',
+  gray = '#e0e0e0',
+  darkgray = '#404148',
+  lightgray = '#efefef'
+}
+-- LuaFormatter on
+
+return {
+  normal = {
+      a = {bg = colors.blue, fg = colors.lightgray, gui = 'bold'},
+      b = {bg = colors.lightgray, fg = colors.blue},
+      c = {bg = colors.gray, fg = colors.white}
+  },
+
+  insert = {
+      a = {bg = colors.cyan, fg = colors.lightgray, gui = 'bold'},
+      b = {bg = colors.lightgray, fg = colors.cyan},
+      c = {bg = colors.gray, fg = colors.white}
+  },
+
+  visual = {
+      a = {bg = colors.magenta, fg = colors.lightgray, gui = 'bold'},
+      b = {bg = colors.lightgray, fg = colors.magenta},
+      c = {bg = colors.gray, fg = colors.white}
+  },
+
+  replace = {
+      a = {bg = colors.red, fg = colors.lightgray, gui = 'bold'},
+      b = {bg = colors.lightgray, fg = colors.red},
+      c = {bg = colors.gray, fg = colors.white}
+  },
+
+  command = {
+      a = {bg = colors.green, fg = colors.lightgray, gui = 'bold'},
+      b = {bg = colors.lightgray, fg = colors.green},
+      c = {bg = colors.gray, fg = colors.white}
+  },
+
+  inactive = {
+      a = {bg = colors.darkgray, fg = colors.lightgray, gui = 'bold'},
+      b = {bg = colors.darkgray, fg = colors.lightgray},
+      c = {bg = colors.darkgray, fg = colors.lightgray}
+  }
+}
