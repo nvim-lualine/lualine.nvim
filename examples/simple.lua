a7f5b14e497b29192179cfa7a39e369b3651acbe
bryantTheCoder <foobryant99@gmail.com>
2021-11-27 11:57:09 +0800

    Update simple.lua

--- examples/simple.lua
+++ examples/simple.lua
@@ -19,15 +19,6 @@ local colors = {
   purple        = '#cd8fe0',
 }
 
-local conditions = {
-  buffer_not_empty = function()
-    return vim.fn.empty(vim.fn.expand '%:t') ~= 1
-  end,
-  hide_in_width = function()
-    return vim.fn.winwidth(0) > 80
-  end,
-}
-
 local function center()
   return '%='
 end
