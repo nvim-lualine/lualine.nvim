a5b3895b57a0ca2e8fec0040afc6cfa81b2a95a4
Cason Adams <casonadams@gmail.com>
2021-12-22 22:39:41 -0700

    Update evil_lualine.lua (#508)
    fix: typeo in evil_lualine example

--- examples/evil_lualine.lua
+++ examples/evil_lualine.lua
@@ -60,7 +60,7 @@ local config = {
   inactive_sections = {
     -- these are to remove the defaults
     lualine_a = {},
-    lualine_v = {},
+    lualine_b = {},
     lualine_y = {},
     lualine_z = {},
     lualine_c = {},
