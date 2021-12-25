9e26823ea6c7361aba3253c8a5c56a6f35b4a0ee
shadmansaleh <13149513+shadmansaleh@users.noreply.github.com>
2021-12-25 16:13:39 +0600

    fixup: error on branch component when stray .git files exist
    fixes #490

--- lua/lualine/components/branch/git_branch.lua
+++ lua/lualine/components/branch/git_branch.lua
@@ -78,9 +78,11 @@ function M.find_git_dir(dir_path)
       elseif git_file_stat.type == 'file' then
         -- separate git-dir or submodule is used
         local file = io.open(git_path)
-        git_dir = file:read()
-        git_dir = git_dir:match 'gitdir: (.+)$'
-        file:close()
+        if file then
+          git_dir = file:read()
+          git_dir = git_dir and git_dir:match 'gitdir: (.+)$'
+          file:close()
+        end
         -- submodule / relative file path
         if git_dir and git_dir:sub(1, 1) ~= sep and not git_dir:match '^%a:.*$' then
           git_dir = git_path:match '(.*).git' .. git_dir
