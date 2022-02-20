77af476305d7f3cbb2d1f9672c14d9204666483c
Michael Lan <michaellan202@gmail.com>
2022-02-20 09:34:50 -0800

    feat: Enable adaptive theme for ayu theme

--- lua/lualine/themes/ayu.lua
+++ lua/lualine/themes/ayu.lua
@@ -2,6 +2,7 @@
 -- MIT license, see LICENSE for more details.
 -- Credit: itchyny(lightline)
 -- License: MIT License
-local style = vim.g.ayucolor or 'dark'
+local background = vim.opt.background:get()
+local style = vim.g.ayucolor or ((background == 'dark') and vim.g.ayuprefermirage and 'mirage' or background)
 
 return require('lualine.themes.ayu_' .. style)
