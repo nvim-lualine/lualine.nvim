bdf7f45f59cc0882820fcd0d896f84d8f06a4541
Jess Telford <jess+github@jes.st>
2022-03-22 16:28:34 +1100

    feat(evil): Display up to 3 lsp names
    When multiple LSP clients are active on a buffer, display the first 3 names in full.
    
    When more than 3 LSP clients are active, display a "+X" suffix (where "X" is the number of additional clients not displayed).

--- examples/evil_lualine.lua
+++ examples/evil_lualine.lua
@@ -158,17 +158,29 @@ ins_left {
 ins_left {
   -- Lsp server name .
   function()
-    local msg = 'No Active Lsp'
     local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
     local clients = vim.lsp.get_active_clients()
     if next(clients) == nil then
-      return msg
+      return 'No Active Lsp'
     end
+    local clientNames = {}
     for _, client in ipairs(clients) do
       local filetypes = client.config.filetypes
       if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
-        return client.name
+        table.insert(clientNames, client.name)
       end
+    end
+    if #clientNames == 0 then
+      return 'No Active Lsp'
+    end
+    local msg = ''
+    local maxClientsToPrint = 3
+    local separator = '·'
+    for i = 1, math.min(maxClientsToPrint, #clientNames) do
+      msg = msg..(i == 1 and '' or separator)..clientNames[i]
+    end
+    if #clientNames > maxClientsToPrint then
+      msg = msg..separator..'+'..(#clientNames - maxClientsToPrint)
     end
     return msg
   end,
