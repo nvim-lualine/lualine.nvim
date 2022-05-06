84e02fec1b8f4600193b115c05c036a5fb685ef4
dmun <davidru01@live.nl>
2022-05-06 14:28:42 +0200

    fix: large file lag

--- lua/lualine/components/filesize.lua
+++ lua/lualine/components/filesize.lua
@@ -1,7 +1,14 @@
 -- Copyright (c) 2020-2021 shadmansaleh
 -- MIT license, see LICENSE for more details.
 local function filesize()
-  local size = vim.fn.wordcount().bytes
+  local file = vim.fn.expand('%:p')
+  if file == nil or #file == 0 then
+    return ''
+  end
+  local size = vim.fn.getfsize(file)
+  if size <= 0 then
+    return ''
+  end
 
   local suffixes = { 'b', 'k', 'm', 'g' }
 
