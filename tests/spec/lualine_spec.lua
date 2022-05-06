6290832d9ab49afcf7b473a2b63ba3a461499802
Marc Jakobi <mrcjk@p40yoga.localdomain>
2022-05-06 21:56:05 +0200

    Add test cases for modes 3 & 4

--- tests/spec/lualine_spec.lua
+++ tests/spec/lualine_spec.lua
@@ -750,6 +750,54 @@ describe('Lualine', function()
         {3:                                                                                                              }|
         ]===])
       end)
+
+      it('can show buffer numbers instead of indices without file names', function()
+        local conf = vim.deepcopy(tab_conf)
+        conf.tabline.lualine_a = { { 'buffers', mode = 3, max_length = 1e3, icons_enabled = false } }
+        vim.cmd('tabe ' .. 'a.txt')
+        vim.cmd('tabe ' .. 'b.txt')
+        require('lualine').setup(conf)
+        require('lualine').statusline()
+        tabline:expect([===[
+        highlights = {
+            1: lualine_a_buffers_inactive = { bg = "#3c3836", bold = true, fg = "#a89984" }
+            2: lualine_transitional_lualine_a_buffers_inactive_to_lualine_a_buffers_active = { bg = "#a89984", fg = "#3c3836" }
+            3: lualine_a_buffers_active = { bg = "#a89984", bold = true, fg = "#282828" }
+            4: lualine_transitional_lualine_a_buffers_active_to_lualine_a_buffers_inactive = { bg = "#3c3836", fg = "#a89984" }
+            5: lualine_c_normal = { bg = "#3c3836", fg = "#a89984" }
+        }
+        |{1: #4  }
+        {2:}
+        {3: 5  }
+        {4:}
+        {1: 8  }
+        {MATCH:{5:%s+}|}
+        ]===])
+      end)
+
+      it('can show buffer numbers instead of indices with file names', function()
+        local conf = vim.deepcopy(tab_conf)
+        conf.tabline.lualine_a = { { 'buffers', mode = 4, max_length = 1e3, icons_enabled = false } }
+        vim.cmd('tabe ' .. 'a.txt')
+        vim.cmd('tabe ' .. 'b.txt')
+        require('lualine').setup(conf)
+        require('lualine').statusline()
+        tabline:expect([===[
+        highlights = {
+            1: lualine_a_buffers_inactive = { bg = "#3c3836", bold = true, fg = "#a89984" }
+            2: lualine_transitional_lualine_a_buffers_inactive_to_lualine_a_buffers_active = { bg = "#a89984", fg = "#3c3836" }
+            3: lualine_a_buffers_active = { bg = "#a89984", bold = true, fg = "#282828" }
+            4: lualine_transitional_lualine_a_buffers_active_to_lualine_a_buffers_inactive = { bg = "#3c3836", fg = "#a89984" }
+            5: lualine_c_normal = { bg = "#3c3836", fg = "#a89984" }
+        }
+        |{1: #4 a.txt }
+        {2:}
+        {3: 5 b.txt }
+        {4:}
+        {1: 8 [No Name] }
+        {MATCH:{5:%s+}|}
+        ]===])
+      end)
     end)
 
     describe('windows component', function()
