3c3c88d05974c67bb1fa71db5c791ea4b48e4172
Oliver Brewka <pymeonlee@gmail.com>
2022-05-25 23:40:48 +0200

    rethemed

--- lua/lualine/themes/violet.lua
+++ lua/lualine/themes/violet.lua
@@ -11,33 +11,35 @@ local colors = {
   gray_4        = '#282828',
   rose          = '#ce537a',
   pink          = '#ff5faf',
-  purple        = '#d75fd7',
+  purple        = '#d75fff',
   darkpurple    = '#875faf',
   aqua          = '#009966',
   yellow        = '#f0f571',
+  accent        = '#52736D',
 }
 
 return {
   normal = {
-    a = {bg = colors.darkpurple, fg = colors.gray_1, gui = 'bold'},
-    b = {bg = colors.gray_4, fg = colors.purple},
-    c = {bg = colors.purple, fg = colors.gray_0}
+    a = {bg = colors.darkpurple, fg = colors.gray_0, gui = 'bold'},
+    b = {bg = colors.purple, fg = colors.gray_4},
+    c = {bg = colors.purple, fg = colors.lightblue}
   },
   insert = {
-    a = {bg = colors.yellow, fg = colors.darkpurple, gui = 'bold'},
-    b = {bg = colors.gray_4, fg = colors.purple},
-    c = {bg = colors.purple, fg = colors.gray_0}
+    a = {bg = colors.yellow, fg = colors.gray_4, gui = 'bold'},
+    b = {bg = colors.purple, fg = colors.gray_4},
+    c = {bg = colors.purple, fg = colors.lightblue_4},
+    z = {bg = colors.accent, fg = colors.lightblue, gui = 'bold'},
   },
   visual = {
-    a = {bg = colors.pink, fg = colors.gray_3, gui = 'bold'},
+    a = {bg = colors.pink, fg = colors.gray_4, gui = 'bold'},
   },
   replace = {
-    a = {bg = colors.rose, fg = colors.gray_0, gui = 'bold'},
+    a = {bg = colors.rose, fg = colors.gray_3, gui = 'bold'},
   },
   command = {
-    a = {bg = colors.gray_4, fg = colors.aqua, gui = 'bold'},
+    a = {bg = colors.aqua, fg = colors.gray_3, gui = 'bold'},
   },
   inactive = {
-    a = {bg = colors.gray_3, fg = colors.rose, gui = 'bold'},
+    a = {bg = colors.accent, fg = colors.lightblue, gui = 'bold'},
   }
 }
