deb90d0568fd029c8d465dc607468f8e7d7eb822
Oliver Brewka <pymeonlee@gmail.com>
2022-05-25 23:22:14 +0200

    edited theme

--- lua/lualine/themes/violet.lua
+++ lua/lualine/themes/violet.lua
@@ -1,36 +1,41 @@
+------
+-- Violet theme inspired by the airline theme and a bugged display of it.
+------
+
 local colors = {
   lightblue     = '#cacfd2',
   gray_0        = '#c6c6c6',
   gray_1        = '#bcbcbc',
   gray_2        = '#4e4e4e',
   gray_3        = '#3a3a3a',
-  accent        = '#5f0000',
+  gray_4        = '#282828',
   rose          = '#ce537a',
   pink          = '#ff5faf',
   purple        = '#d75fd7',
   darkpurple    = '#875faf',
   aqua          = '#009966',
+  yellow        = '#f0f571',
 }
 
 return {
   normal = {
     a = {bg = colors.darkpurple, fg = colors.gray_1, gui = 'bold'},
-    b = {bg = colors.gray_2, fg = colors.purple},
-    c = {bg = colors.gray_3, fg = colors.gray_0}
+    b = {bg = colors.gray_4, fg = colors.purple},
+    c = {bg = colors.purple, fg = colors.gray_0}
   },
   insert = {
-    a = {bg = colors.aqua, fg = colors.lightblue, gui = 'bold'},
-    b = {bg = colors.gray_2, fg = colors.purple},
-    c = {bg = colors.gray_3, fg = colors.gray_0}
+    a = {bg = colors.yellow, fg = colors.darkpurple, gui = 'bold'},
+    b = {bg = colors.gray_4, fg = colors.purple},
+    c = {bg = colors.purple, fg = colors.gray_0}
   },
   visual = {
-    a = {bg = colors.pink, fg = colors.accent, gui = 'bold'},
+    a = {bg = colors.pink, fg = colors.gray_3, gui = 'bold'},
   },
   replace = {
     a = {bg = colors.rose, fg = colors.gray_0, gui = 'bold'},
   },
   command = {
-    a = {bg = colors.gray_3, fg = colors.aqua, gui = 'bold'},
+    a = {bg = colors.gray_4, fg = colors.aqua, gui = 'bold'},
   },
   inactive = {
     a = {bg = colors.gray_3, fg = colors.rose, gui = 'bold'},
