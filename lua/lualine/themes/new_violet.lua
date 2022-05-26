be650183eebabfed9b875c36012b99e009bab54c
Oliver Brewka <pymeonlee@gmail.com>
2022-05-26 02:52:51 +0200

    rework

--- lua/lualine/themes/new_violet.lua
+++ lua/lualine/themes/new_violet.lua
@@ -5,18 +5,14 @@
 local colors = {
   lightblue     = '#cacfd2',
   white         = '#ffffff',
-  gray_0        = '#c6c6c6',
-  gray_1        = '#bcbcbc',
-  gray_2        = '#4e4e4e',
-  gray_3        = '#3a3a3a',
-  gray_4        = '#282828',
+  gray_0        = '#3a3a3a',
+  gray_1        = '#282828',
   rose          = '#ce537a',
   pink          = '#ff5faf',
   purple        = '#d75fff',
   darkpurple    = '#875faf',
   aqua          = '#009966',
   yellow        = '#f0f571',
-  accent        = '#52736D',
 }
 
 return {
@@ -37,7 +33,8 @@ return {
   visual = {
     a = {bg = colors.yellow, fg = colors.gray_4, gui = 'bold'},
     b = {bg = colors.purple, fg = colors.white},
-    c = {bg = colors.darkpurple, fg = colors.white}
+    c = {bg = colors.purple, fg = colors.white},
+    y = {bg = colors.darkpurple, fg = colors.white},
   },
   replace = {
     a = {bg = colors.gray_4, fg = colors.purple, gui = 'bold'},
