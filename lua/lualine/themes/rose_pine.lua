38313bfbc9125189fd983e0bcd89ba5aa8d2d798
A. Taha Baki <atahabaki@pm.me>
2022-06-27 14:30:31 +0300

    Rosé Pine

---
+++ lua/lualine/themes/rose_pine.lua
@@ -0,0 +1,43 @@
+local colors = {
+  gray       = '#1f1d2e',
+  lightgray  = '#26233a',
+  orange     = '#ebbcba',
+  red        = '#eb6f92',
+  yellow     = '#f6c177',
+  green      = '#9ccfd8',
+  white      = '#e0def4',
+  black      = '#191724',
+}
+
+return {
+  normal = {
+    a = { bg = colors.black, fg = colors.white, gui = 'bold' },
+    b = { bg = colors.gray, fg = colors.white },
+    c = { bg = colors.lightgray, fg = colors.white },
+  },
+  insert = {
+    a = { bg = colors.green, fg = colors.black, gui = 'bold' },
+    b = { bg = colors.lightgray, fg = colors.white },
+    c = { bg = colors.gray, fg = colors.white },
+  },
+  visual = {
+    a = { bg = colors.yellow, fg = colors.black, gui = 'bold' },
+    b = { bg = colors.lightgray, fg = colors.white },
+    c = { bg = colors.gray, fg = colors.white },
+  },
+  replace = {
+    a = { bg = colors.red, fg = colors.black, gui = 'bold' },
+    b = { bg = colors.lightgray, fg = colors.white },
+    c = { bg = colors.gray, fg = colors.white },
+  },
+  command = {
+    a = { bg = colors.orange, fg = colors.black, gui = 'bold' },
+    b = { bg = colors.lightgray, fg = colors.white },
+    c = { bg = colors.gray, fg = colors.white },
+  },
+  inactive = {
+    a = { bg = colors.gray, fg = colors.white, gui = 'bold' },
+    b = { bg = colors.lightgray, fg = colors.white },
+    c = { bg = colors.gray, fg = colors.white },
+  },
+}
