655411fb7aa3cf4d46094132d684d815453f5043
shadmansaleh <shadmansaleh@users.noreply.github.com>
2022-07-15 11:53:29 +0000

    chore: autogen (vimdocs+formating)

--- lua/lualine/extensions/mundo.lua
+++ lua/lualine/extensions/mundo.lua
@@ -4,17 +4,16 @@ M.sections = {
   lualine_a = {
     function()
       local ft = vim.opt_local.filetype:get()
-      return (ft == "Mundo")     and "Change tree"
-          or (ft == "MundoDiff") and "Change diff"
-    end
+      return (ft == 'Mundo') and 'Change tree' or (ft == 'MundoDiff') and 'Change diff'
+    end,
   },
-  lualine_y = { "progress" },
-  lualine_z = { "location" },
+  lualine_y = { 'progress' },
+  lualine_z = { 'location' },
 }
 
 M.filetypes = {
-  "Mundo",
-  "MundoDiff",
+  'Mundo',
+  'MundoDiff',
 }
 
 return M
