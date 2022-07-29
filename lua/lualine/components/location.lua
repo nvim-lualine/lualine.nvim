0fe35b1990119793f4d7158f122eb238e58ac57e
shadmansaleh <13149513+shadmansaleh@users.noreply.github.com>
2022-07-29 15:56:09 +0600

    make location & progress components behave in ingnored filetypes

--- lua/lualine/components/location.lua
+++ lua/lualine/components/location.lua
@@ -1,7 +1,9 @@
 -- Copyright (c) 2020-2021 hoob3rt
 -- MIT license, see LICENSE for more details.
 local function location()
-  return '%3l:%-2v'
+  local line = vim.fn.line('.')
+  local col = vim.fn.col('.')
+  return string.format('%3d:%-2d', line, col)
 end
 
 return location
