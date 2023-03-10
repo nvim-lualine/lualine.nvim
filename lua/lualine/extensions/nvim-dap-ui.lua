898b634900a960f4c736cbcf0eafc918fa6dfd9d
Wassim Metallaoui <wassim@metallaoui.com>
2023-03-10 08:18:33 -0600

    feat: add dap server status to nvim-dap-ui extension
    The current status of the debug server can be retrieved via
    require('dap').status(). It's helpful to always see it on screen while
    debugging.

--- lua/lualine/extensions/nvim-dap-ui.lua
+++ lua/lualine/extensions/nvim-dap-ui.lua
@@ -1,9 +1,27 @@
 -- MIT license, see LICENSE for more details.
 -- Extension for nvim-dap-ui
+
+local dap_lua, _ = pcall(require, 'dap')
+
+local function dap_status()
+  if not dap_lua then
+    return ''
+  end
+
+  local status = require('dap').status()
+
+  if status == nil or status == '' then
+    return ''
+  else
+    return 'DAP: ' .. status
+  end
+end
+
 local M = {}
 
 M.sections = {
   lualine_a = { { 'filename', file_status = false } },
+  lualine_x = { dap_status },
 }
 
 M.filetypes = {
