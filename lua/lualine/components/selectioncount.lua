a1200a8630059852e8bac339fd9c45ff3ec6c891
dmyTRUEk <25669613+dmyTRUEk@users.noreply.github.com>
2023-04-17 11:55:44 +0300

    fix: `selectioncount` with `virtualedit` (#1012)

--- lua/lualine/components/selectioncount.lua
+++ lua/lualine/components/selectioncount.lua
@@ -1,7 +1,9 @@
 local function selectioncount()
   local mode = vim.fn.mode(true)
-  local line_start, col_start = vim.fn.line('v'), vim.fn.col('v')
-  local line_end, col_end = vim.fn.line('.'), vim.fn.col('.')
+  local pos_start = vim.fn.getpos('v')
+  local pos_end = vim.fn.getpos('.')
+  local line_start, col_start = pos_start[2], pos_start[3] + pos_start[4]
+  local line_end, col_end = pos_end[2], pos_end[3] + pos_end[4]
   if mode:match('') then
     return string.format('%dx%d', math.abs(line_start - line_end) + 1, math.abs(col_start - col_end) + 1)
   elseif mode:match('V') or line_start ~= line_end then
