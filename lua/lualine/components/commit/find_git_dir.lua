aeb40179ac0b8de72f3b829a2ac9b7f74a73c77b
Grzegorz Bednarski <grzegorzbednarski@fastmail.com>
2023-04-23 22:29:39 +0200

    chore: splitted commits_status into multiple modules

---
+++ lua/lualine/components/commit/find_git_dir.lua
@@ -0,0 +1,53 @@
+-- os specific path separator
+local sep = package.config:sub(1, 1)
+
+local M = {}
+
+local git_dir_cache = {} -- Stores git paths that we already know of, map file dir to git_dir
+
+function M.find_git_dir(dir_path)
+    -- get file dir so we can search from that dir
+    local file_dir = dir_path or vim.fn.expand('%:p:h')
+    local root_dir = file_dir
+    local git_dir
+    -- Search upward for .git file or folder
+    while root_dir do
+        if git_dir_cache[root_dir] then
+            git_dir = git_dir_cache[root_dir]
+            break
+        end
+        local git_path = root_dir .. sep .. '.git'
+        local git_file_stat = vim.loop.fs_stat(git_path)
+        if git_file_stat then
+            if git_file_stat.type == 'directory' then
+                git_dir = git_path
+            elseif git_file_stat.type == 'file' then
+                -- separate git-dir or submodule is used
+                local file = io.open(git_path)
+                if file then
+                    git_dir = file:read()
+                    git_dir = git_dir and git_dir:match('gitdir: (.+)$')
+                    file:close()
+                end
+                -- submodule / relative file path
+                if git_dir and git_dir:sub(1, 1) ~= sep and not git_dir:match('^%a:.*$') then
+                    git_dir = git_path:match('(.*).git') .. git_dir
+                end
+            end
+            if git_dir then
+                local head_file_stat = vim.loop.fs_stat(git_dir .. sep .. 'HEAD')
+                if head_file_stat and head_file_stat.type == 'file' then
+                    break
+                else
+                    git_dir = nil
+                end
+            end
+        end
+        root_dir = root_dir:match('(.*)' .. sep .. '.-')
+    end
+
+    git_dir_cache[file_dir] = git_dir
+    return git_dir
+end
+
+return M
---
+++ lua/lualine/components/commit/repo_watcher.lua
@@ -0,0 +1,338 @@
+local require = require('lualine_require').require
+local jobs = require('lualine.components.commit.jobs')
+local git_dir = require('lualine.components.commit.find_git_dir')
+local find_git_dir = git_dir.find_git_dir
+
+-- os specific path separator
+local sep = package.config:sub(1, 1)
+
+---Validates args for `throttle()` and  `debounce()`.
+local function td_validate(fn, ms)
+    vim.validate {
+        fn = { fn, 'f' },
+        ms = {
+            ms,
+            function(ms)
+                return type(ms) == 'number' and ms > 0
+            end,
+            "number > 0",
+        },
+    }
+end
+
+--- Throttles a function on the trailing edge. Automatically
+--- `schedule_wrap()`s.
+---
+--@param fn (function) Function to throttle
+--@param timeout (number) Timeout in ms
+--@param last (boolean, optional) Whether to use the arguments of the last
+---call to `fn` within the timeframe. Default: Use arguments of the first call.
+--@returns (function, timer) Throttled function and timer. Remember to call
+---`timer:close()` at the end or you will leak memory!
+function throttle_trailing(fn, ms, last)
+    td_validate(fn, ms)
+    local timer = vim.loop.new_timer()
+    local running = false
+
+    local wrapped_fn
+    if not last then
+        function wrapped_fn(...)
+            if not running then
+                local argv = { ... }
+                local argc = select('#', ...)
+
+                timer:start(ms, 0, function()
+                    running = false
+                    pcall(vim.schedule_wrap(fn), unpack(argv, 1, argc))
+                end)
+                running = true
+            end
+        end
+    else
+        local argv, argc
+        function wrapped_fn(...)
+            argv = { ... }
+            argc = select('#', ...)
+
+            if not running then
+                timer:start(ms, 0, function()
+                    running = false
+                    pcall(vim.schedule_wrap(fn), unpack(argv, 1, argc))
+                end)
+                running = true
+            end
+        end
+    end
+    return wrapped_fn, timer
+end
+
+local RepoWatcher = {}
+
+function RepoWatcher:new(git_dir, master_name, opts)
+    local timer = vim.loop.new_timer()
+    local repo = {
+        opts = opts,
+        dir = git_dir,
+        git_cwd = git_dir:sub(1, -6), -- cut  '/.git' suffix
+        ref = '',
+        master_name = opts.master_name,
+        branch_name = '',
+        origin_set = false,
+        no_upstream = false,
+        timer = timer,
+        file_changed = sep ~= '\\' and vim.loop.new_fs_event() or vim.loop.new_fs_poll(),
+        branch_tip_changed = sep ~= '\\' and vim.loop.new_fs_event() or vim.loop.new_fs_poll(),
+        remote_branch_tip_changed = sep ~= '\\' and vim.loop.new_fs_event() or vim.loop.new_fs_poll(),
+        master_commit_count = 0,
+        unpushed_commit_count = 0,
+        unpulled_commit_count = 0,
+        current_branch_conflict = false,
+        master_branch_conflict = false,
+        _update = throttle_trailing(function(self)
+            if self.opts.diff_against_master then
+                self:update_master()
+            end
+            self:update_current()
+        end, 50, false)
+    }
+    setmetatable(repo, self)
+    self.__index = self
+
+    repo:start_watch()
+    return repo
+end
+
+function RepoWatcher:start_watch()
+    local init = function()
+        self:watch_head()
+        self:watch_ref()
+
+        if self.origin_set then
+            -- don't watch for changes in remote branch tip if there is
+            -- no origin.
+            self:watch_remote_ref()
+        end
+
+        self.timer:start(0, self.opts.interval, vim.schedule_wrap(function()
+            if self.opts.diff_against_master then
+                self:sync_and_update_master()
+            end
+            self:sync_and_update_current()
+        end))
+    end
+
+    jobs.check_origin(self.git_cwd, function(success)
+        if success then
+            self.origin_set = true
+        else
+            -- set special values, as zero (in sync) is misleading
+            self.unpushed_commit_count = -1
+            self.unpulled_commit_count = -1
+        end
+
+        if self.opts.findout_master_name and self.origin_set then
+            jobs.get_master_name(self.git_cwd, function(success, master_name)
+                if not success then
+                    print("unable to get master name")
+                    return
+                end
+                self.maste_name = master_name
+                init()
+            end)
+            return
+        end
+
+        init()
+    end)
+end
+
+function RepoWatcher:watch_head()
+    self.file_changed:stop()
+
+    self:update()
+
+    local head_file = self.dir .. sep .. 'HEAD'
+
+    local lines = {}
+    for line in io.lines(head_file) do
+        lines[#lines + 1] = line
+    end
+    local ref = lines[1]:gsub("ref: ", "")
+    local branch_name = ref:gsub("refs/heads/", "")
+    if ref ~= self.ref then
+        self.ref = ref
+        self.branch_name = branch_name
+        self.no_upstream = false -- reset as we don't know, check is done on first diff attempt
+        -- if the head changes, we should reset the watch_ref
+        self:watch_ref()
+        if self.origin_set then
+            self:watch_remote_ref()
+        end
+    end
+
+    self.file_changed:start(
+        head_file,
+        sep ~= '\\' and {} or 1000,
+        vim.schedule_wrap(function()
+            self:watch_head()
+        end)
+    )
+end
+
+function RepoWatcher:watch_ref()
+    self:update()
+
+    self.branch_tip_changed:stop()
+    local branch_tip_file = self.dir .. sep .. self.ref
+    self.branch_tip_changed:start(
+        branch_tip_file,
+        sep ~= '\\' and {} or 1000,
+        vim.schedule_wrap(function()
+            self:watch_ref()
+        end)
+    )
+end
+
+function RepoWatcher:watch_remote_ref()
+    self:update()
+
+    self.remote_branch_tip_changed:stop()
+    local remote_branch_tip_file = self.dir .. sep .. self.ref:gsub("heads", "remotes/origin")
+    self.remote_branch_tip_changed:start(
+        remote_branch_tip_file,
+        sep ~= '\\' and {} or 1000,
+        vim.schedule_wrap(function()
+            self.watch_remote_ref()
+        end)
+    )
+end
+
+function RepoWatcher:restart_watch()
+    -- restart timer
+    self.timer:again()
+end
+
+function RepoWatcher:update()
+    self:_update()
+end
+
+function RepoWatcher:update_master()
+    local source = 'origin/' .. self.master_name
+    if not self.origin_set then
+        -- fallback to compare with local branch
+        source = self.master_name
+    end
+
+    -- current branch may not be set yet
+    if self.branch_name == '' or self.branch_name == self.master_name then
+        -- no need to display sync with master info in this case.
+        self.master_commit_count = -1
+        self.master_branch_conflict = false
+        return
+    end
+
+    jobs.commit_diff(self.git_cwd, source, '^@', function(success, count)
+        if not success then
+            print("git log failed")
+            return
+        end
+
+        -- remove this if statement after implementing debounce on
+        -- fs watch
+        -- now, this function may return after figuring out the
+        -- current branch name
+        if not (self.branch_name == '' or self.branch_name == self.master_name) then
+            -- no need to display sync with master info in this case.
+            self.master_commit_count = count
+        end
+    end)
+
+    if self.origin_set then
+        jobs.check_for_conflict(self.git_cwd, source, '@', function(success, has_conflict)
+            if not success then
+                print("failed to check for conflict")
+                return
+            end
+            self.master_branch_conflict = has_conflict
+        end)
+    end
+end
+
+function RepoWatcher:update_current()
+    if not self.origin_set or self.no_upstream then
+        -- there is noting to compare with
+        return
+    end
+
+    jobs.commit_diff(self.git_cwd, '@', '^@{upstream}', function(success, count, err)
+        if not success then
+            if string.find(err, "no upstream configured") then
+                self.no_upstream = true
+                self.unpushed_commit_count = -1
+                self.current_branch_conflict = false
+                return
+            end
+            print("git log failed")
+            return
+        end
+
+        self.unpushed_commit_count = count
+    end)
+
+    jobs.commit_diff(self.git_cwd, '^@', '@{upstream}', function(success, count, err)
+        if not success then
+            if string.find(err, "no upstream configured") then
+                self.no_upstream = true
+                self.unpulled_commit_count = -1
+                self.current_branch_conflict = false
+                return
+            end
+            print("git log failed")
+            return
+        end
+
+        self.unpulled_commit_count = count
+    end)
+
+    jobs.check_for_conflict(self.git_cwd, '@{upstream}', '@', function(success, has_conflict)
+        if not success then
+            print("failed to check for conflict on current branch")
+            return
+        end
+        self.current_branch_conflict = has_conflict
+    end)
+end
+
+function RepoWatcher:sync_and_update_master()
+    if not self.origin_set then
+        -- there is noting to sync with
+        -- just update, curent branch may be not master
+        self:update_master()
+        return
+    end
+
+    jobs.fetch_branch(self.git_cwd, self.master_name, function(success)
+        if not success then
+            print("failed to fetch branch " .. self.opts.master_name)
+            return
+        end
+        self:update_master()
+    end)
+end
+
+function RepoWatcher:sync_and_update_current()
+    if not self.origin_set then
+        -- there is noting to sync and compare with
+        return
+    end
+
+    jobs.fetch_branch(self.git_cwd, '@', function(success)
+        if not success then
+            print("failed to fetch branch @")
+            return
+        end
+        self:update_current()
+    end)
+end
+
+return { RepoWatcher = RepoWatcher }
