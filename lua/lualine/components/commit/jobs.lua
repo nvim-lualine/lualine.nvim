291270f38231756da05de9b9bc58494bc880f7fa
Grzegorz Bednarski <grzegorzbednarski@fastmail.com>
2023-04-27 16:32:25 +0200

    format: formatted code with stylua

--- lua/lualine/components/commit/jobs.lua
+++ lua/lualine/components/commit/jobs.lua
@@ -3,98 +3,91 @@ local job = require('lualine.utils.job')
 local M = {}
 
 function M._run_job(cmd, cwd, callback)
-    local output = ''
-    local err = ''
+  local output = ''
+  local err = ''
 
-    local j = job({
-        cmd = {
-            'sh', '-c', cmd
-        },
-        cwd = cwd,
-        stdout_buffered = true,
-        stderr_buffered = true,
-        on_stdout = function(_, data)
-            output = table.concat(data, '\n')
-        end,
-        on_stderr = function(_, data)
-            err = table.concat(data, '\n')
-        end,
-        on_exit = function(_, exit_code)
-            callback(exit_code, output, err)
-        end
-    })
+  local j = job {
+    cmd = {
+      'sh',
+      '-c',
+      cmd,
+    },
+    cwd = cwd,
+    stdout_buffered = true,
+    stderr_buffered = true,
+    on_stdout = function(_, data)
+      output = table.concat(data, '\n')
+    end,
+    on_stderr = function(_, data)
+      err = table.concat(data, '\n')
+    end,
+    on_exit = function(_, exit_code)
+      callback(exit_code, output, err)
+    end,
+  }
 
-    if j then
-        j:start()
-    end
+  if j then
+    j:start()
+  end
 end
 
 function M.check_origin(cwd, callback)
-    M._run_job('git remote show', cwd, function(exit_code, output, _)
-        output = output:gsub('\n', '')
-        if exit_code ~= 0 or output ~= 'origin' then
-            callback(false)
-            return
-        end
+  M._run_job('git remote show', cwd, function(exit_code, output, _)
+    output = output:gsub('\n', '')
+    if exit_code ~= 0 or output ~= 'origin' then
+      callback(false)
+      return
+    end
 
-        callback(true)
-    end)
+    callback(true)
+  end)
 end
 
 function M.get_master_name(cwd, callback)
-    M._run_job("git remote show origin | grep 'HEAD branch' | cut -d' ' -f5", cwd, function(exit_code, output, _)
-        output = output:gsub('\n', '')
-        if exit_code ~= 0 then
-            callback(false)
-            return
-        end
-        callback(true, output)
-    end)
+  M._run_job("git remote show origin | grep 'HEAD branch' | cut -d' ' -f5", cwd, function(exit_code, output, _)
+    output = output:gsub('\n', '')
+    if exit_code ~= 0 then
+      callback(false)
+      return
+    end
+    callback(true, output)
+  end)
 end
 
 function M.fetch_branch(cwd, name, callback)
-    local cmd = string.format([[git fetch origin %s]], name)
-    M._run_job(cmd, cwd, function(exit_code, _, _)
-        if exit_code ~= 0 then
-            callback(false)
-            return
-        end
-        callback(true)
-    end)
+  local cmd = string.format([[git fetch origin %s]], name)
+  M._run_job(cmd, cwd, function(exit_code, _, _)
+    if exit_code ~= 0 then
+      callback(false)
+      return
+    end
+    callback(true)
+  end)
 end
 
 function M.check_for_conflict(cwd, source, target, callback)
-    local cmd = string.format(
-        [[git merge-tree `git merge-base %s %s` %s %s | grep '<<<<<<<']],
-        source,
-        target,
-        target,
-        source
-    )
-    M._run_job(cmd, cwd, function(exit_code, _, _)
-        if exit_code ~= 0 then
-            -- grep that finds nothing returns exit code 1
-            callback(true, false)
-            return
-        end
-        callback(true, true)
-    end)
+  local cmd =
+    string.format([[git merge-tree `git merge-base %s %s` %s %s | grep '<<<<<<<']], source, target, target, source)
+  M._run_job(cmd, cwd, function(exit_code, _, _)
+    if exit_code ~= 0 then
+      -- grep that finds nothing returns exit code 1
+      callback(true, false)
+      return
+    end
+    callback(true, true)
+  end)
 end
 
 function M.commit_diff(cwd, source, target, callback)
-    local cmd = string.format(
-        [[git log --oneline %s %s]],
-        source,
-        target
-    )
-    M._run_job(cmd, cwd, function(exit_code, output, err)
-        if exit_code ~= 0 then
-            callback(false, -1, err)
-            return
-        end
-        local _, commit_count = output:gsub('\n', '\n')
-        callback(true, commit_count)
-    end)
+  local cmd = string.format([[git log --oneline %s %s]], source, target)
+  M._run_job(cmd, cwd, function(exit_code, output, err)
+    if exit_code ~= 0 then
+      callback(false, -1, err)
+      return
+    end
+    local _, commit_count = output:gsub('\n', '\n')
+    callback(true, commit_count)
+  end)
 end
 
 return M
--- lua/lualine/components/commit/status.lua
+++ lua/lualine/components/commit/status.lua
@@ -5,65 +5,65 @@ local utils = require('lualine.utils.utils')
 local find_git_dir = require('lualine.components.commit.find_git_dir')
 local RepoWatcher = require('lualine.components.commit.repo_watcher')
 
-local git_repo_cache = {}  -- Stores initialized RepoWatchers
+local git_repo_cache = {} -- Stores initialized RepoWatchers
 local current_git_dir = '' -- Used to distinguish if the git_dir really changed
 
 function M.watch_repo(dir_path)
-    local git_dir = find_git_dir(dir_path)
+  local git_dir = find_git_dir(dir_path)
 
-    if git_dir == nil or current_git_dir == git_dir then
-        return -- noting to do
-    end
+  if git_dir == nil or current_git_dir == git_dir then
+    return -- noting to do
+  end
 
-    -- Stop watching on all repos before watching new repository
-    for _, v in pairs(git_repo_cache) do
-        v:stop_watch()
-    end
-    local git_repo = git_repo_cache[git_dir]
-    if git_repo == nil then
-        git_repo = RepoWatcher:new(git_dir, {
-            master_name = M.opts.master_name,
-            fetch_interval = M.opts.fetch_interval,
-            diff_against_master = M.opts.diff_against_master,
-            findout_master_name = M.opts.findout_master_name,
-        })
-        git_repo_cache[git_dir] = git_repo
-    else
-        git_repo:restart_watch()
-    end
+  -- Stop watching on all repos before watching new repository
+  for _, v in pairs(git_repo_cache) do
+    v:stop_watch()
+  end
+  local git_repo = git_repo_cache[git_dir]
+  if git_repo == nil then
+    git_repo = RepoWatcher:new(git_dir, {
+      master_name = M.opts.master_name,
+      fetch_interval = M.opts.fetch_interval,
+      diff_against_master = M.opts.diff_against_master,
+      findout_master_name = M.opts.findout_master_name,
+    })
+    git_repo_cache[git_dir] = git_repo
+  else
+    git_repo:restart_watch()
+  end
 
-    current_git_dir = git_dir
+  current_git_dir = git_dir
 end
 
 function M.init(opts)
-    M.opts = opts
-    M.watch_repo()
-    utils.define_autocmd('BufEnter', "lua require'lualine.components.commit.status'.watch_repo()")
+  M.opts = opts
+  M.watch_repo()
+  utils.define_autocmd('BufEnter', "lua require'lualine.components.commit.status'.watch_repo()")
 end
 
 function M.status(bufnr)
-    local file_dir
-    if bufnr then
-        local bufname = vim.fn.bufname(bufnr)
-        file_dir = vim.fn.fnamemodify(bufname, ':p:h')
-    else
-        file_dir = vim.fn.expand('%:p:h')
-    end
+  local file_dir
+  if bufnr then
+    local bufname = vim.fn.bufname(bufnr)
+    file_dir = vim.fn.fnamemodify(bufname, ':p:h')
+  else
+    file_dir = vim.fn.expand('%:p:h')
+  end
 
-    local result = {}
-    local git_dir = find_git_dir(file_dir)
-    if git_dir == nil then
-        return result
-    end
-
-    local repo = git_repo_cache[git_dir]
-    if repo == nil then
-        return result
-    end
-
-    table.insert(result, { repo.master_commit_count, repo.master_branch_conflict })
-    table.insert(result, { repo.unpulled_commit_count, repo.unpushed_commit_count, repo.current_branch_conflict })
+  local result = {}
+  local git_dir = find_git_dir(file_dir)
+  if git_dir == nil then
     return result
+  end
+
+  local repo = git_repo_cache[git_dir]
+  if repo == nil then
+    return result
+  end
+
+  table.insert(result, { repo.master_commit_count, repo.master_branch_conflict })
+  table.insert(result, { repo.unpulled_commit_count, repo.unpushed_commit_count, repo.current_branch_conflict })
+  return result
 end
 
 return M
