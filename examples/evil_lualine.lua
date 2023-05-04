8912bea65de93a56b1f70cdb7c3c26f9cce30394
Eugene Oliveros <yujin@jinyuz.dev>
2023-05-04 23:08:04 +0800

    fix: replace deprecated nerdfont icons (#1035)
    Co-authored-by: Shadman <13149513+shadmansaleh@users.noreply.github.com>

--- examples/evil_lualine.lua
+++ examples/evil_lualine.lua
@@ -200,7 +200,7 @@ ins_right {
 ins_right {
   'diff',
   -- Is it me or the symbol for modified us really weird
-  symbols = { added = ' ', modified = '柳 ', removed = ' ' },
+  symbols = { added = ' ', modified = '󰝤 ', removed = ' ' },
   diff_color = {
     added = { fg = colors.green },
     modified = { fg = colors.orange },
