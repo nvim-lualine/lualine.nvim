1e3184b849b9b3ba8f0626457c43c03e6876dfbc
Karma Riuk <riukkarma@gmail.com>
2023-06-06 20:42:55 +0200

    Fixed mistake, the order of defintion was incorrect

--- lua/lualine/extensions/texcount.lua
+++ lua/lualine/extensions/texcount.lua
@@ -1,14 +1,14 @@
 local M = {}
 
-M.filetypes = { 'tex' }
-M.sections = require('lualine').get_config().sections
-local orignal_sections = require('lualine').get_config().sections.lualine_x
-M.sections.lualine_x = vim.tbl_extend('keep', { n_words_component }, orignal_sections)
-
 local n_words = '-'
 local function n_words_component()
   return n_words .. ' words'
 end
+
+M.filetypes = { 'tex' }
+M.sections = require('lualine').get_config().sections
+local orignal_sections = require('lualine').get_config().sections.lualine_x
+M.sections.lualine_x = vim.tbl_extend('keep', { n_words_component }, orignal_sections)
 
 local texcount_group = vim.api.nvim_create_augroup('TexCount', { clear = true })
 vim.api.nvim_create_autocmd({ 'BufWritePost', 'BufWinEnter' }, {
