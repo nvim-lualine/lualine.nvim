1dc8b44423078fda163cc0abb800963eb4c8343e
shadmansaleh <shadmansaleh@users.noreply.github.com>
2023-10-20 03:32:59 +0000

    chore: autogen (vimdocs+formating)

--- lua/lualine/extensions/ctrlspace.lua
+++ lua/lualine/extensions/ctrlspace.lua
@@ -1,10 +1,18 @@
 local M = {}
 
 M.sections = {
-  lualine_a = { function() return vim.fn['ctrlspace#context#Configuration']().Symbols.CS end },
+  lualine_a = {
+    function()
+      return vim.fn['ctrlspace#context#Configuration']().Symbols.CS
+    end,
+  },
   lualine_b = { 'ctrlspace#api#StatuslineModeSegment' },
   lualine_y = { 'ctrlspace#api#StatuslineTabSegment' },
-  lualine_z = { function() return 'CtrlSpace' end },
+  lualine_z = {
+    function()
+      return 'CtrlSpace'
+    end,
+  },
 }
 
 M.filetypes = { 'ctrlspace' }
