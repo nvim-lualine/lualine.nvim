b428a84a2d6aac01043d9516b837ad28912dd99e
Grzegorz Bednarski <grzegorzbednarski@fastmail.com>
2023-10-22 13:17:30 +0200

    refactor: use lua to parse command output

--- lua/lualine/components/commit/jobs.lua
+++ lua/lualine/components/commit/jobs.lua
@@ -7,11 +7,7 @@ function M._run_job(cmd, cwd, callback)
   local err = ''
 
   local j = job {
-    cmd = {
-      'sh',
-      '-c',
-      cmd,
-    },
+    cmd = cmd,
     cwd = cwd,
     stdout_buffered = true,
     stderr_buffered = true,
@@ -32,7 +28,7 @@ function M._run_job(cmd, cwd, callback)
 end
 
 function M.check_origin(cwd, callback)
-  M._run_job('git remote show', cwd, function(exit_code, output, _)
+  M._run_job([[git remote show]], cwd, function(exit_code, output, _)
     output = output:gsub('\n', '')
     if exit_code ~= 0 or output ~= 'origin' then
       callback(false)
@@ -44,13 +40,13 @@ function M.check_origin(cwd, callback)
 end
 
 function M.get_master_name(cwd, callback)
-  M._run_job("git remote show origin | grep 'HEAD branch' | cut -d' ' -f5", cwd, function(exit_code, output, _)
-    output = output:gsub('\n', '')
+  M._run_job([[git remote show origin]], cwd, function(exit_code, output, _)
     if exit_code ~= 0 then
       callback(false)
       return
     end
-    callback(true, output)
+    local _, _, name = string.find(output, "HEAD branch: (%S+)")
+    callback(true, name)
   end)
 end
 
@@ -66,15 +62,17 @@ function M.fetch_branch(cwd, name, callback)
 end
 
 function M.check_for_conflict(cwd, source, target, callback)
-  local cmd =
-    string.format([[git merge-tree `git merge-base %s %s` %s %s | grep '<<<<<<<']], source, target, target, source)
-  M._run_job(cmd, cwd, function(exit_code, _, _)
+  local cmd = string.format(
+    [[git merge-tree `git merge-base %s %s` %s %s]],
+    source, target, target, source)
+  M._run_job(cmd, cwd, function(exit_code, output, _)
     if exit_code ~= 0 then
-      -- grep that finds nothing returns exit code 1
       callback(true, false)
       return
     end
-    callback(true, true)
+
+    local found = string.find(output, "<<<<<<<")
+    callback(true, found ~= nil)
   end)
 end
 
