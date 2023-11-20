b7a163b12aa9a67f6968c6a7e7850df959f394fc
Sam-programs <None>
2023-11-20 15:12:30 +0300

    fix formatting

--- lua/lualine/utils/mode.lua
+++ lua/lualine/utils/mode.lua
@@ -4,42 +4,42 @@ local Mode = {}
 
 -- stylua: ignore
 Mode.map = {
-   ['n']     = 'NORMAL',
-   ['no']    = 'O-PENDING',
-   ['nov']   = 'O-PENDING',
-   ['noV']   = 'O-PENDING',
-   ['no\22'] = 'O-PENDING',
-   ['niI']   = 'NORMAL',
-   ['niR']   = 'NORMAL',
-   ['niV']   = 'NORMAL',
-   ['nt']    = 'NORMAL',
-   ['ntT']   = 'NORMAL',
-   ['v']     = 'VISUAL',
-   ['vs']    = 'VISUAL',
-   ['V']     = 'V-LINE',
-   ['Vs']    = 'V-LINE',
-   ['\22']   = 'V-BLOCK',
-   ['\22s']  = 'V-BLOCK',
-   ['s']     = 'SELECT',
-   ['S']     = 'S-LINE',
-   ['\19']   = 'S-BLOCK',
-   ['i']     = 'INSERT',
-   ['ic']    = 'INSERT',
-   ['ix']    = 'INSERT',
-   ['R']     = 'REPLACE',
-   ['Rc']    = 'REPLACE',
-   ['Rx']    = 'REPLACE',
-   ['Rv']    = 'V-REPLACE',
-   ['Rvc']   = 'V-REPLACE',
-   ['Rvx']   = 'V-REPLACE',
-   ['c']     = 'COMMAND',
-   ['cv']    = 'EX',
-   ['ce']    = 'EX',
-   ['r']     = 'REPLACE',
-   ['rm']    = 'MORE',
-   ['r?']    = 'CONFIRM',
-   ['!']     = 'SHELL',
-   ['t']     = 'TERMINAL',
+  ['n']     = 'NORMAL',
+  ['no']    = 'O-PENDING',
+  ['nov']   = 'O-PENDING',
+  ['noV']   = 'O-PENDING',
+  ['no\22'] = 'O-PENDING',
+  ['niI']   = 'NORMAL',
+  ['niR']   = 'NORMAL',
+  ['niV']   = 'NORMAL',
+  ['nt']    = 'NORMAL',
+  ['ntT']   = 'NORMAL',
+  ['v']     = 'VISUAL',
+  ['vs']    = 'VISUAL',
+  ['V']     = 'V-LINE',
+  ['Vs']    = 'V-LINE',
+  ['\22']   = 'V-BLOCK',
+  ['\22s']  = 'V-BLOCK',
+  ['s']     = 'SELECT',
+  ['S']     = 'S-LINE',
+  ['\19']   = 'S-BLOCK',
+  ['i']     = 'INSERT',
+  ['ic']    = 'INSERT',
+  ['ix']    = 'INSERT',
+  ['R']     = 'REPLACE',
+  ['Rc']    = 'REPLACE',
+  ['Rx']    = 'REPLACE',
+  ['Rv']    = 'V-REPLACE',
+  ['Rvc']   = 'V-REPLACE',
+  ['Rvx']   = 'V-REPLACE',
+  ['c']     = 'COMMAND',
+  ['cv']    = 'EX',
+  ['ce']    = 'EX',
+  ['r']     = 'REPLACE',
+  ['rm']    = 'MORE',
+  ['r?']    = 'CONFIRM',
+  ['!']     = 'SHELL',
+  ['t']     = 'TERMINAL',
 }
 
 local last_mode = 'NORMAL'
@@ -55,12 +55,14 @@ vim.api.nvim_create_autocmd({ 'CmdlineEnter' }, {
 
 ---@return string current mode name
 function Mode.get_mode()
-  local mode_code = vim.api.nvim_get_mode().mode
-  local mode = Mode.map[mode_code]
   -- if a mapping is not done don't react to mode changes
+  -- still reacts for command mappings that aren't silent see above
   if vim.fn.getchar(1) ~= 0 then
     return last_mode
   end
+
+  local mode_code = vim.api.nvim_get_mode().mode
+  local mode = Mode.map[mode_code]
   if mode == nil then
     mode = mode_code
   end
