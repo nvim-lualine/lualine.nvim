f59c84d272287dba1c88df362066cd9186718ff9
sergiuser1 <sergiu.ojog@tuta.io>
2024-04-11 19:34:11 +0200

    feat: Add BOM indicator to encoding component

--- lua/lualine/components/encoding.lua
+++ lua/lualine/components/encoding.lua
@@ -1,7 +1,12 @@
 -- Copyright (c) 2020-2021 hoob3rt
 -- MIT license, see LICENSE for more details.
 local function encoding()
-  return vim.opt.fileencoding:get()
+  local result = vim.opt.fileencoding:get()
+  if vim.opt.bomb:get() then
+    result = result .. ' [BOM]'
+  end
+
+  return result
 end
 
 return encoding
