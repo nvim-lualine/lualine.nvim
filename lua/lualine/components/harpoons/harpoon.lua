2c1e12690541dec7b45c4665b2f82dae8b5d1117
Michael Lehenauer <michael@lehenauer.com>
2024-07-17 02:38:06 +0200

    add component for harpoon

---
+++ lua/lualine/components/harpoons/harpoon.lua
@@ -0,0 +1,205 @@
+local Harpoon = require('lualine.utils.class'):extend()
+local harpoon_plug = require('harpoon')
+
+local modules = require('lualine_require').lazy_require {
+  highlight = 'lualine.highlight',
+  utils = 'lualine.utils.utils',
+}
+
+---initialize a new harpoon from opts
+---@param opts table
+function Harpoon:init(opts)
+  assert(opts.bufnr or opts.hpnr, 'Cannot create Harpoon without bufnr')
+  self.hpnr = opts.hpnr
+  self.bufnr = opts.bufnr
+  self.options = opts.options
+  self.highlights = opts.highlights
+  self:get_props()
+end
+
+function Harpoon:is_current()
+  return self.bufnr and vim.api.nvim_get_current_buf() == self.bufnr
+end
+
+function Harpoon:is_alternate()
+  return vim.fn.bufnr('#') == self.hpnr and not self:is_current()
+end
+
+local function get_file_extension(url)
+  return url:match('^.+%.(.+)$')
+end
+
+local function starts_with(str, start)
+  return str:sub(1, #start) == start
+end
+
+---setup icons, modified status for harpoon
+function Harpoon:get_props()
+  if self.bufnr then
+    self.file = modules.utils.stl_escape(vim.api.nvim_buf_get_name(self.bufnr))
+    self.buftype = vim.api.nvim_get_option_value('buftype', { buf = self.bufnr })
+    self.filetype = vim.api.nvim_get_option_value('filetype', { buf = self.bufnr })
+  else
+    self.file = harpoon_plug:list().items[self.hpnr].value
+    self.buftype = nil
+    self.filetype = get_file_extension(self.file)
+    if starts_with(self.file, 'oil') then
+      self.filetype = 'oil'
+    end
+  end
+
+  -- remove the oil prefix
+  if self.filetype == 'oil' then
+    self.file = string.sub(self.file, 7)
+  end
+
+  local modified = self.bufnr and vim.api.nvim_get_option_value('modified', { buf = self.bufnr })
+  self.modified_icon = modified and self.options.symbols.modified or ''
+  self.alternate_file_icon = self:is_alternate() and self.options.symbols.alternate_file or ''
+  self.icon = ''
+  if self.options.icons_enabled then
+    local dev
+    local status, _ = pcall(require, 'nvim-web-devicons')
+    if not status then
+      dev, _ = '', ''
+    elseif self.filetype == 'TelescopePrompt' then
+      dev, _ = require('nvim-web-devicons').get_icon('telescope')
+    elseif self.filetype == 'fugitive' then
+      dev, _ = require('nvim-web-devicons').get_icon('git')
+    elseif self.filetype == 'vimwiki' then
+      dev, _ = require('nvim-web-devicons').get_icon('markdown')
+    elseif self.buftype == 'terminal' then
+      dev, _ = require('nvim-web-devicons').get_icon('zsh')
+    elseif vim.fn.isdirectory(self.file) == 1 then
+      dev, _ = self.options.symbols.directory, nil
+    else
+      dev, _ = require('nvim-web-devicons').get_icon(self.file, vim.fn.expand(self.filetype))
+    end
+    if dev then
+      self.icon = dev .. ' '
+    end
+  end
+end
+
+---returns line configured for handling mouse click
+---@param name string
+---@return string
+function Harpoon:configure_mouse_click(name)
+  if self.hpnr then
+    return string.format('%%%s@LualineSwitchHarpoon@%s%%T', self.hpnr, name)
+  else
+    return name
+  end
+end
+
+---returns rendered harpoon
+---@return string
+function Harpoon:render()
+  local name = self:name()
+  if self.options.fmt then
+    name = self.options.fmt(name or '', self)
+  end
+
+  if self.ellipse then -- show ellipsis
+    name = '...'
+  else
+    name = self:apply_mode(name)
+  end
+  name = Harpoon.apply_padding(name, self.options.padding)
+  self.len = vim.fn.strchars(name)
+
+  -- setup for mouse clicks
+  local line = self:configure_mouse_click(name)
+  -- apply highlight
+  line = modules.highlight.component_format_highlight(self.highlights[(self.current and 'active' or 'inactive')])
+    .. line
+
+  -- apply separators
+  if self.options.self.section < 'x' and not self.first then
+    local sep_before = self:separator_before()
+    line = sep_before .. line
+    self.len = self.len + vim.fn.strchars(sep_before)
+  elseif self.options.self.section >= 'x' and not self.last then
+    local sep_after = self:separator_after()
+    line = line .. sep_after
+    self.len = self.len + vim.fn.strchars(sep_after)
+  end
+  return line
+end
+
+---apply separator before current harpoon
+---@return string
+function Harpoon:separator_before()
+  if self.current or self.aftercurrent then
+    return '%Z{' .. self.options.section_separators.left .. '}'
+  else
+    return self.options.component_separators.left
+  end
+end
+
+---apply separator after current harpoon
+---@return string
+function Harpoon:separator_after()
+  if self.current or self.beforecurrent then
+    return '%z{' .. self.options.section_separators.right .. '}'
+  else
+    return self.options.component_separators.right
+  end
+end
+
+---returns name of current harpoon after filtering special harpoons
+---@return string
+function Harpoon:name()
+  if self.options.filetype_names[self.filetype] then
+    return self.options.filetype_names[self.filetype]
+  elseif self.buftype == 'help' then
+    return 'help:' .. vim.fn.fnamemodify(self.file, ':t:r')
+  elseif self.buftype == 'terminal' then
+    local match = string.match(vim.split(self.file, ' ')[1], 'term:.*:(%a+)')
+    return match ~= nil and match or vim.fn.fnamemodify(vim.env.SHELL, ':t')
+  elseif self.buftype == 'quickfix' then
+    local is_loclist = 0 ~= vim.fn.getloclist(0, { filewinid = 1 }).filewinid
+    return is_loclist and 'Location list' or 'Quickfix List'
+  elseif vim.fn.isdirectory(self.file) == 1 then
+    return vim.fn.fnamemodify(self.file, ':p:.')
+  elseif self.file == '' then
+    return '[No Name]'
+  end
+
+  local name
+  if self.options.show_filename_only then
+    name = vim.fn.fnamemodify(self.file, ':t')
+  else
+    name = vim.fn.pathshorten(vim.fn.fnamemodify(self.file, ':p:.'))
+  end
+  if self.options.hide_filename_extension then
+    name = vim.fn.fnamemodify(name, ':r')
+  end
+  return name
+end
+
+---adds spaces to left and right
+function Harpoon.apply_padding(str, padding)
+  local l_padding, r_padding = 1, 1
+  if type(padding) == 'number' then
+    l_padding, r_padding = padding, padding
+  elseif type(padding) == 'table' then
+    l_padding, r_padding = padding.left or 0, padding.right or 0
+  end
+  return string.rep(' ', l_padding) .. str .. string.rep(' ', r_padding)
+end
+
+function Harpoon:apply_mode(name)
+  if self.options.mode == 0 then
+    return string.format('%s%s%s%s', self.alternate_file_icon, self.icon, name, self.modified_icon)
+  end
+
+  if self.options.mode == 1 then
+    return string.format('%s%s %s%s', self.alternate_file_icon, self.hpnr or '', self.icon, self.modified_icon)
+  end
+
+  -- if self.options.mode == 2 then
+  return string.format('%s%s %s%s%s', self.alternate_file_icon, self.hpnr or '', self.icon, name, self.modified_icon)
+end
+
+return Harpoon
---
+++ lua/lualine/components/harpoons/init.lua
@@ -0,0 +1,245 @@
+-- Copyright (c) 2020-2021 shadmansaleh
+-- MIT license, see LICENSE for more details.
+local require = require('lualine_require').require
+local Harpoon = require('lualine.components.harpoons.harpoon')
+local M = require('lualine.component'):extend()
+local highlight = require('lualine.highlight')
+local harpoon_plug = require('harpoon')
+
+local default_options = {
+  show_filename_only = true,
+  hide_filename_extension = false,
+  show_modified_status = true,
+  mode = 0,
+  max_length = 0,
+  filetype_names = {
+    TelescopePrompt = 'Telescope',
+    dashboard = 'Dashboard',
+    packer = 'Packer',
+    fzf = 'FZF',
+    alpha = 'Alpha',
+  },
+  use_mode_colors = false,
+  harpoons_color = {
+    active = nil,
+    inactive = nil,
+  },
+  symbols = {
+    modified = ' ●',
+    alternate_file = '#',
+    directory = '',
+  },
+}
+
+-- This function is duplicated in tabs
+---returns the proper hl for harpoon in section. Used for setting default highlights
+---@param section string name of section harpoons component is in
+---@param is_active boolean
+---@return string hl name
+local function get_hl(section, is_active)
+  local suffix = is_active and highlight.get_mode_suffix() or '_inactive'
+  local section_redirects = {
+    lualine_x = 'lualine_c',
+    lualine_y = 'lualine_b',
+    lualine_z = 'lualine_a',
+  }
+  if section_redirects[section] then
+    section = highlight.highlight_exists(section .. suffix) and section or section_redirects[section]
+  end
+  return section .. suffix
+end
+
+function M:init(options)
+  M.super.init(self, options)
+  -- if use_mode_colors is set, use a function so that the colors update
+  local default_active = options.use_mode_colors
+      and function()
+        return get_hl('lualine_' .. options.self.section, true)
+      end
+    or get_hl('lualine_' .. options.self.section, true)
+  default_options.harpoons_color = {
+    active = default_active,
+    inactive = get_hl('lualine_' .. options.self.section, false),
+  }
+  self.options = vim.tbl_deep_extend('keep', self.options or {}, default_options)
+  if self.options.component_name == 'harpoons' then
+    self.highlights = {
+      active = self:create_hl(self.options.harpoons_color.active, 'active'),
+      inactive = self:create_hl(self.options.harpoons_color.inactive, 'inactive'),
+    }
+  end
+end
+
+function M:new_harpoon(hpnr, bufnr)
+  return Harpoon:new {
+    hpnr = hpnr,
+    bufnr = bufnr,
+    options = self.options,
+    highlights = self.highlights,
+  }
+end
+
+function M:harpoons()
+  local buffers = {}
+  local harpoons = {}
+  for _, b in ipairs(vim.api.nvim_list_bufs()) do
+    if vim.api.nvim_buf_is_loaded(b) then
+      table.insert(buffers, b)
+    end
+  end
+
+  local currIsHarpoon = false
+  for i, h in pairs(harpoon_plug:list():display()) do
+    if h ~= '' then
+      local bufnr = nil
+      for _, buf in pairs(buffers) do
+        local bufPath = vim.api.nvim_buf_get_name(buf)
+        if string.find(bufPath, h, 1, true) then
+          bufnr = buf
+          currIsHarpoon = true
+        end
+      end
+      harpoons[i] = self:new_harpoon(i, bufnr)
+    end
+  end
+
+  if not currIsHarpoon and vim.fn.bufnr() then
+    table.insert(harpoons, self:new_harpoon(nil, vim.fn.bufnr()))
+  end
+
+  return harpoons
+end
+
+function M:update_status()
+  local data = {}
+  local harpoons = self:harpoons()
+  local current = -2
+  -- mark the first, last, current, before current, after current harpoons
+  -- for rendering
+  if harpoons[1] then
+    harpoons[1].first = true
+  end
+  if harpoons[#harpoons] then
+    harpoons[#harpoons].last = true
+  end
+  for i, harpoon in ipairs(harpoons) do
+    if harpoon:is_current() then
+      harpoon.current = true
+      current = i
+    end
+  end
+  if harpoons[current - 1] then
+    harpoons[current - 1].beforecurrent = true
+  end
+  if harpoons[current + 1] then
+    harpoons[current + 1].aftercurrent = true
+  end
+
+  local max_length = self.options.max_length
+  if type(max_length) == 'function' then
+    max_length = max_length(self)
+  end
+
+  if max_length == 0 then
+    max_length = math.floor(2 * vim.o.columns / 3)
+  end
+  local total_length
+  for i, harpoon in pairs(harpoons) do
+    if harpoon.current then
+      current = i
+    end
+  end
+
+  -- start drawing from current harpoon and draw left and right of it until
+  -- all harpoons are drawn or max_length has been reached.
+  if current == -2 then
+    local b = self:new_harpoon(nil, vim.fn.bufnr())
+    b.current = true
+    if self.options.self.section < 'x' then
+      b.last = true
+      if #harpoons > 0 then
+        harpoons[#harpoons].last = nil
+      end
+      harpoons[#harpoons + 1] = b
+      current = #harpoons
+    else
+      b.first = true
+      if #harpoons > 0 then
+        harpoons[1].first = nil
+      end
+      table.insert(harpoons, 1, b)
+      current = 1
+    end
+  end
+  local current_harpoon = harpoons[current]
+  data[#data + 1] = current_harpoon:render()
+  total_length = current_harpoon.len
+  local i = 0
+  local before, after
+  while true do
+    i = i + 1
+    before = harpoons[current - i]
+    after = harpoons[current + i]
+    local rendered_before, rendered_after
+    if before == nil and after == nil then
+      break
+    end
+    -- draw left most undrawn harpoon if fits in max_length
+    if before then
+      rendered_before = before:render()
+      total_length = total_length + before.len
+      if total_length > max_length then
+        break
+      end
+      table.insert(data, 1, rendered_before)
+    end
+    -- draw right most undrawn harpoon if fits in max_length
+    if after then
+      rendered_after = after:render()
+      total_length = total_length + after.len
+      if total_length > max_length then
+        break
+      end
+      data[#data + 1] = rendered_after
+    end
+  end
+  -- draw ellipsis (...) on relevant sides if all harpoons don't fit in max_length
+  if total_length > max_length then
+    if before ~= nil then
+      before.ellipse = true
+      before.first = true
+      table.insert(data, 1, before:render())
+    end
+    if after ~= nil then
+      after.ellipse = true
+      after.last = true
+      data[#data + 1] = after:render()
+    end
+  end
+
+  return table.concat(data)
+end
+
+function M:draw()
+  self.status = ''
+  self.applied_separator = ''
+
+  if self.options.cond ~= nil and self.options.cond() ~= true then
+    return self.status
+  end
+  local status = self:update_status()
+  if type(status) == 'string' and #status > 0 then
+    self.status = status
+    self:apply_section_separators()
+    self:apply_separator()
+  end
+  return self.status
+end
+
+vim.cmd([[
+  function! LualineSwitchHarpoon(hpnr, mouseclicks, mousebutton, modifiers)
+    execute ":lua require(\"harpoon\"):list():select(" . a:hpnr . ")"
+  endfunction
+]])
+
+return M
