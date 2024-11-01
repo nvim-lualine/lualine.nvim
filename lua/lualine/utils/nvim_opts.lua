5ca49096b59e211d1365dfadbd68ef06f76e57c1
shadmansaleh <13149513+shadmansaleh@users.noreply.github.com>
2024-11-01 22:11:24 +0600

    remove debounce

--- lua/lualine/utils/nvim_opts.lua
+++ lua/lualine/utils/nvim_opts.lua
@@ -1,12 +1,10 @@
 local M = {}
 
-local DEBOUNCE_DELAY = 50 -- debouce for 100ms
-
 -- keeps backup of options that we cahge so we can restore it.
 -- format:
 -- options {
 --   global = <1> {
---     name = {prev, set, to_set}
+--     name = {prev, set}
 --   },
 --   buffer = {
 --     buf1 = <1>,
@@ -20,7 +18,6 @@ local DEBOUNCE_DELAY = 50 -- debouce for 100ms
 ---@class LualineNvimOptCacheOptStore
 ---@field prev any
 ---@field set any
----@field to_set any[]
 ---@alias LualineNvimOptCacheOpt table<string, LualineNvimOptCacheOptStore>
 ---@class LualineNvimOptCache
 ---@field global LualineNvimOptCacheOptStore[]
@@ -43,51 +40,18 @@ local function set_opt(name, val, getter_fn, setter_fn, cache_tbl)
     return
   end
   if cache_tbl[name] == nil then
-    cache_tbl[name] = {prev = nil, set = nil, to_set = {}}
+    cache_tbl[name] = {}
   end
-  table.insert(cache_tbl[name].to_set, val)
 
-  local defered_setter = function()
-    if #cache_tbl[name].to_set > 1 then
-      local freq = {}
-      for _,v in ipairs(cache_tbl[name]) do
-        if freq[v] == nil then freq[v] = 0 end
-        freq[v] = freq[v] + 1
-      end
-      local prev = cache_tbl[name].prev
-      if prev ~= nil then
-        if freq[prev] == nil then freq[prev] = 0 end
-        freq[prev] = freq[prev] + 1
-      end
-
-      -- pick the most frequent set in the debouce period with bias toward prev
-      local most_freq = prev ~= nil and prev or cache_tbl[name][1]
-      for k, v in pairs(freq) do
-        if v  >= freq[most_freq] then
-          most_freq = k
-        end
-      end
-      cache_tbl[name].to_set = {most_freq}
+  if cache_tbl[name].set ~= cur then
+    if type(cur) ~= 'string' or not cur:find('lualine') then
+      cache_tbl[name].prev = cur
     end
-    if cache_tbl[name].to_set[#cache_tbl[name].to_set] ~= val then return end
-    cache_tbl[name].to_set = {}
-    if cache_tbl[name].set ~= cur then
-      if type(cur) ~= 'string' or not cur:find('lualine') then
-        cache_tbl[name].prev = cur
-      end
-    end
-    cache_tbl[name].set = val
-    setter_fn(name, val)
-    if name == 'statusline' or name == 'winbar' then vim.cmd('redrawstatus') end
-    if name == 'tabline' then vim.cmd('redrawtabline') end
   end
-
-  if cache_tbl.prev == nil or cache_tbl.prev == '' then
-    -- when setting for the first time no need to delay
-    defered_setter()
-  else
-    vim.defer_fn(defered_setter, DEBOUNCE_DELAY)
-  end
+  cache_tbl[name].set = val
+  setter_fn(name, val)
+  if name == 'statusline' or name == 'winbar' then vim.cmd('redrawstatus') end
+  if name == 'tabline' then vim.cmd('redrawtabline') end
 end
 
 -- set a option value
