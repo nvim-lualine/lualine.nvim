e4cf501c99c7ddb65a88f3b01a7d4ab618137a1f
tvercruyssen <vercruyssen.thibeau@gmail.com>
2025-03-28 21:42:13 +0100

    Update function to retrieve diagnostic signs to nvim version 0.10+

--- lua/lualine/components/diagnostics/config.lua
+++ lua/lualine/components/diagnostics/config.lua
@@ -2,18 +2,23 @@ local require = require('lualine_require').require
 local utils = require('lualine.utils.utils')
 local M = {}
 
-local function get_sign(name)
-    local sign = vim.fn.sign_getdefined("DiagnosticSign" .. name)[1]
-    return (sign and sign.text) or nil
+local function get_sign(severity)
+  local config = vim.diagnostic.config() or {}
+  local signs = config.signs or {}
+  if type(signs) == 'function' then
+    signs = signs(0, 0)
+  end
+  return (type(signs) == 'table' and signs.text and signs.text[severity]) or nil
 end
+-- https://github.com/folke/trouble.nvim/blob/85bedb7eb7fa331a2ccbecb9202d8abba64d37b3/lua/trouble/format.lua#L97
 
 -- default symbols for diagnostics component
 M.symbols = {
   icons = {
-    error = get_sign('Error') or '󰅚 ', -- x000f015a
-    warn = get_sign('Warn') or '󰀪 ', -- x000f002a
-    info = get_sign('Info') or '󰋽 ', -- x000f02fd
-    hint = get_sign('Hint') or '󰌶 ', -- x000f0336
+    error = get_sign(vim.diagnostic.severity.ERROR) or '󰅚 ', -- x000f015a
+    warn = get_sign(vim.diagnostic.severity.WARN) or '󰀪 ', -- x000f002a
+    info = get_sign(vim.diagnostic.severity.INFO) or '󰋽 ', -- x000f02fd
+    hint = get_sign(vim.diagnostic.severity.HINT) or '󰌶 ', -- x000f0336
   },
   no_icons = { error = 'E:', warn = 'W:', info = 'I:', hint = 'H:' },
 }
