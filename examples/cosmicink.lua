57e74ba704fac2670faf1a061b8cfb95a7896a6d
zai1208 <zai1208@outlook.com>
2025-08-26 15:11:42 +1000

    Update vim.lsp.get_active_clients to vim.lsp.get_clients as get_active_clients is deprecated

--- examples/cosmicink.lua
+++ examples/cosmicink.lua
@@ -486,7 +486,7 @@ ins_right {
   function()
     local msg = 'No Active Lsp'
     local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
-    local clients = vim.lsp.get_active_clients()
+    local clients = vim.lsp.get_clients()
     if next(clients) == nil then
       return msg
     end
