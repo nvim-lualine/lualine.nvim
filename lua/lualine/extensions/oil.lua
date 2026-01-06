27ae7a790a495c05f1308f8f21691288d8b12b25
Alexandre Beaufays <beaufays.alexandre@gmail.com>
2026-01-06 22:27:09 +0100

    Make oil extension behave like default configuration
    Prior to this change, the oil extension would show an empty status line
    except for the file name in lualine_a.
    
    This change make the status line behave like the default one, and sets
    the filename / lualine_c as the path to the shown directory.

--- lua/lualine/extensions/oil.lua
+++ lua/lualine/extensions/oil.lua
@@ -2,17 +2,18 @@
 
 local M = {}
 
-M.sections = {
-  lualine_a = {
+local config = require('lualine.config')
+
+M.sections = vim.deepcopy(config.get_config().sections)
+M.sections.lualine_c = {
     function()
-      local ok, oil = pcall(require, 'oil')
-      if ok then
-        return vim.fn.fnamemodify(oil.get_current_dir(), ':~')
-      else
-        return ''
-      end
+        local ok, oil = pcall(require, 'oil')
+        if ok then
+            return vim.fn.fnamemodify(oil.get_current_dir(), ':~')
+        else
+            return ''
+        end
     end,
-  },
 }
 
 M.filetypes = { 'oil' }
